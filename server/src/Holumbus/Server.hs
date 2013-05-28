d26ff44c32f5dddb700d95f79b3f1e5052230e04
Chris Reumann <chrisreumann@googlemail.com>
2013-05-28 21:35:25 +0200

    document datatype and json tests

--- server/src/Holumbus/Server.hs
+++ server/src/Holumbus/Server.hs
@@ -14,26 +14,57 @@ import qualified Data.Text as T
 --import qualified Text.Blaze.Html5.Attributes as A
 import Data.Monoid (mconcat)
 import Data.Aeson hiding (json)
+import Data.Map
 import qualified Data.Aeson as J
 
 -- import holu 1.3.2 types
-import Holumbus.Index.Common 
+--import Holumbus.Index.Common 
+
+--
+-- incoming documents:
+--
+-- Description contains data for persistent storage of document
+-- Words contains data to for index structures
+--
+data ApiDocument = ApiDocument 
+  { apiDocUri :: Uri
+  , apiDocDesc :: Desc
+  , apiDocWords :: Words
+  }
+type Desc = Map String String
+type Words = Map Context WordList
+type Context = String
+type WordList = Map Word [Int]
+type Word = String
+type Uri = String
+--
+-- outgoing documents
+--
+-- search results contain serialized documents:
+--
+data Document = Document (Uri, Desc)
+--
+--
+data Customer = Customer T.Text T.Text Int
 
 
--- entity to test json 
-data Customer = Customer {
-  name :: T.Text ,
-  address :: T.Text,
-  age :: Int
-}
-
--- from/to json instances
-instance ToJSON Customer where
-  toJSON (Customer n a age) = object 
-    [ "name" .= n
-    , "address" .= a
-    , "age" .= age
+-- we dont really need this...
+instance ToJSON ApiDocument where
+  toJSON (ApiDocument uri desc words) = object
+    [ "uri" .= uri
+    , "desc" .= toJSON desc 
+    , "words" .= toJSON words
     ]
+
+-- we need this - but ... how???
+
+--instance FromJSON ApiDocument where
+--  parseJSON v = do
+--    o <- parseJSON v
+--    uri <- v .: "uri"
+--    ds <- parseJSON 
+
+
 
 instance FromJSON Customer where
   parseJSON (Object v) = Customer 
@@ -44,20 +75,21 @@ instance FromJSON Customer where
 
 -- server itself
 start = scotty 3000 $ do
-  let customers = [ Customer "Chris" "Musterstraße 34" 28
-                  , Customer "Tom" "Dorfstraße 23" 30
+  let documents = [ ApiDocument "1" 
+                      (Data.Map.fromList [("title", "document1"), ("content", "... ... ... ")]) 
+                      (Data.Map.fromList [("context1", Data.Map.fromList [("hallo", [2,6,7])])])
                   ] 
 
   middleware logStdoutDev
 
   get "/" $ text "mainpage"
 
-  get "/customers" $ json customers
+ -- get "/customers" $ json customers
 
-  get "/customer/:index" $ do
+  get "/documents/:index" $ do
     index <- param "index"
-    if length customers > index
-      then json $ customers !! index
+    if length documents > index
+      then json $ documents !! index
       else text $ "index out of range"
  
   get "/:word" $ do
