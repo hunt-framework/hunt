df7b70a906cbd6b5d288054be3a7921966a700e3
Chris Reumann <chrisreumann@googlemail.com>
2013-06-17 00:25:30 +0200

    some ideas about ApiDocument

--- server/src/Holumbus/Server/Common.hs
+++ server/src/Holumbus/Server/Common.hs
@@ -15,7 +15,33 @@ type Words        = Map Context WordList
 -- | map from word to a list of occurrences
 type WordList     = Map Word [Position]
 
--- | type the server receives to add/modify/delete? in index
+-- | type the server receives to add/modify/delete? in indexi
+-- @FIXME 
+-- this is way too much work to do for the client implementation
+-- we should consider receiving something like an entity with
+-- some kind of meta information attached. then our implemented
+-- analyzer extracts the words and occurrences to build the index.
+--
+-- an indexed field would be stored as a context in the index
+--
+-- may look like this:
+--
+-- data ApiDocument = ApiDocument URI [Field]
+--
+-- data Field = Field {
+--    name  :: Text
+--    value :: Text
+--    meta  :: FieldMeta
+-- }
+--
+-- data FieldMeta = FieldMeta {
+--    index = IndexType
+--    ...
+--    ...
+-- }
+--
+-- data IndexType = NO_INDEX | ANALYZER1 | ANALYZER2
+--
 data ApiDocument = ApiDocument
   { apiDocUri   :: URI
   , apiDocDesc  :: Description
