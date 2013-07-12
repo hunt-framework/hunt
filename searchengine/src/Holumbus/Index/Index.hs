515eb491f5d079c4b3de5c5d6891c7a588717a6e
Ulf Sauer <ulf.sauer@gmail.com>
2013-07-12 22:54:01 +0200

    Add implementation type parameter to the Index, implement merge etc.
    - The added type parameter represents the implementation of the index
      and allows to write a function to access the specific implementation
    - Merge and subtract functions are implemented using this accessor

--- searchengine/src/Holumbus/Index/Index.hs
+++ searchengine/src/Holumbus/Index/Index.hs
@@ -28,101 +28,105 @@ import           Holumbus.Index.Compression     as C
 --
 -- external interface
 
-sizeWords             :: Index -> Int
+sizeWords             :: Index i -> Int
 sizeWords             = _sizeWords
 
 -- | Returns a list of all contexts avaliable in the index.
-contexts              :: Index -> [Context]
+contexts              :: Index i -> [Context]
 contexts              = _contexts
 
 -- | Returns the occurrences for every word. A potentially expensive operation.
-allWords              :: Index -> Context -> RawResult
+allWords              :: Index i -> Context -> RawResult
 allWords              = _allWords
 
 -- | Searches for words beginning with the prefix in a given context (case-sensitive).
-prefixCase            :: Index -> Context -> Text -> RawResult
+prefixCase            :: Index i -> Context -> Text -> RawResult
 prefixCase            = _prefixCase
 
 -- | Searches for words beginning with the prefix in a given context (case-insensitive).
-prefixNoCase          :: Index -> Context -> Text -> RawResult
+prefixNoCase          :: Index i -> Context -> Text -> RawResult
 prefixNoCase          = _prefixNoCase
 
 -- | Searches for and exact word in a given context (case-sensitive).
-lookupCase            :: Index -> Context -> Text -> RawResult
+lookupCase            :: Index i -> Context -> Text -> RawResult
 lookupCase            = _lookupCase
 
 -- | Searches for and exact word in a given context (case-insensitive).
-lookupNoCase          :: Index -> Context -> Text -> RawResult
+lookupNoCase          :: Index i -> Context -> Text -> RawResult
 lookupNoCase          = _lookupNoCase
 
 -- | Insert occurrences.
-insertOccurrences     :: Context -> Word -> Occurrences -> Index -> Index
+insertOccurrences     :: Context -> Word -> Occurrences -> Index i -> Index i
 insertOccurrences     = \c w o i -> _insertOccurrences i c w o
 
 -- | Delete occurrences.
-deleteOccurrences     :: Context -> Word -> Occurrences -> Index -> Index
+deleteOccurrences     :: Context -> Word -> Occurrences -> Index i -> Index i
 deleteOccurrences     = \c w o i -> _deleteOccurrences i c w o
 
 -- | Insert a position for a single document.
-insertPosition        :: Context -> Word -> DocId -> Position -> Index -> Index
+insertPosition        :: Context -> Word -> DocId -> Position -> Index i -> Index i
 insertPosition        = \c w d p i -> _insertPosition i c w d p
 --insertPosition c w d p i      ,, insertOccurrences c w (singletonOccurrence d p) Index
 
 -- | Delete a position for a single document.
-deletePosition        :: Context -> Word -> DocId -> Position -> Index -> Index
+deletePosition        :: Context -> Word -> DocId -> Position -> Index i -> Index i
 deletePosition        = \c w d p i -> _deletePosition i c w d p
 --deletePosition c w d p i      ,, deleteOccurrences c w (singletonOccurrence d p) Index
 
 -- | Delete documents completely (all occurrences).
-deleteDocsById        :: Set DocId -> Index -> Index
+deleteDocsById        :: Set DocId -> Index i -> Index i
 deleteDocsById        = \ds i -> _deleteDocsById i ds
 
 -- | Merges two indexes.
---mergeIndexes          :: Index -> Index -> Index
---mergeIndexes i1 i2    = _mergeIndexes i1 i2
+mergeIndexes          :: Index i -> Index i -> Index i
+mergeIndexes          = _mergeIndexes
 
--- | Substract one index from another.
---substractIndexes      :: Index -> Index -> Index
---substractIndexes      = _substractIndexes
+-- | Subtract one index from another.
+subtractIndexes       :: Index i -> Index i -> Index i
+subtractIndexes       = _subtractIndexes
 
 -- | Splitting an index by its contexts.
-splitByContexts       :: Index -> Int -> [Index]
+splitByContexts       :: Index i -> Int -> [Index i]
 splitByContexts       = _splitByContexts
 
 -- | Splitting an index by its documents.
-splitByDocuments      :: Index -> Int -> [Index]
+splitByDocuments      :: Index i -> Int -> [Index i]
 splitByDocuments      = _splitByDocuments
 
 -- | Splitting an index by its words.
-splitByWords          :: Index -> Int -> [Index]
+splitByWords          :: Index i -> Int -> [Index i]
 splitByWords          = _splitByWords
 
 -- | Update document id's (e.g. for renaming documents). If the function maps two different id's
 -- to the same new id, the two sets of word positions will be merged if both old id's are present
 -- in the occurrences for a word in a specific context.
-updateDocIds          :: (Context -> Word -> DocId -> DocId) -> Index -> Index
+updateDocIds          :: (Context -> Word -> DocId -> DocId) -> Index i -> Index i
 updateDocIds          = \f i -> _updateDocIds i f
 
 -- | Update document id's with a simple injective editing function.
-updateDocIds'         :: (DocId -> DocId) -> Index -> Index
+updateDocIds'         :: (DocId -> DocId) -> Index i -> Index i
 updateDocIds'         = \f i -> _updateDocIds' i f
 --updateDocIds' f               ,, updateDocIds (const . const $ f)
 
--- Convert an Index to a list. Can be used for easy conversion between different index
+-- | Convert an Index to a list. Can be used for easy conversion between different index
 -- implementations
-toList                :: Index -> [(Context, Word, Occurrences)]
+toList                :: Index i -> [(Context, Word, Occurrences)]
 toList                = _toList
+
+-- | The index implementation
+impl                  :: Index i -> i
+impl                  = _impl
 
 -- XXX: fromList is not in the index data type
 -- Create an Index from a list. Can be used for easy conversion between different index
 -- implementations. Needs an empty index as first argument
-fromList              :: [(Context, Word, Occurrences)] -> Index
+fromList              :: [(Context, Word, Occurrences)] -> Index Inverted
 fromList              = newIndex . fromList'
 --fromList e                    ,, foldl (\i (c,w,o) -> insertOccurrences c w o i) e
 
 -- ----------------------------------------------------------------------------
 
-data Index = Ix
+data Index i = Ix
     {
     -- | Returns the number of unique words in the index.
       _sizeWords                     :: Int
@@ -146,49 +150,48 @@ data Index = Ix
     , _lookupNoCase                  :: Context -> Text -> RawResult
 
     -- | Insert occurrences.
-    , _insertOccurrences             :: Context -> Word -> Occurrences -> Index
+    , _insertOccurrences             :: Context -> Word -> Occurrences -> Index i
 
     -- | Delete occurrences.
-    , _deleteOccurrences             :: Context -> Word -> Occurrences -> Index
+    , _deleteOccurrences             :: Context -> Word -> Occurrences -> Index i
 
     -- | Insert a position for a single document.
-    , _insertPosition                :: Context -> Word -> DocId -> Position -> Index
+    , _insertPosition                :: Context -> Word -> DocId -> Position -> Index i
     --insertPosition c w d p i      = insertOccurrences c w (singletonOccurrence d p) Index
 
     -- | Delete a position for a single document.
-    , _deletePosition                :: Context -> Word -> DocId -> Position -> Index
+    , _deletePosition                :: Context -> Word -> DocId -> Position -> Index i
     --deletePosition c w d p i      = deleteOccurrences c w (singletonOccurrence d p) Index
 
     -- | Delete documents completely (all occurrences).
-    , _deleteDocsById                :: Set DocId -> Index
+    , _deleteDocsById                :: Set DocId -> Index i
 
     -- | Merges two indexes.
-    --, _mergeIndexes                  :: Index -> Index
+    , _mergeIndexes                  :: Index i -> Index i
 
-    -- | Substract one index from another.
-    --, _substractIndexes              :: Index -> Index
+    -- | Subtract one index from another.
+    , _subtractIndexes               :: Index i -> Index i
 
     -- | Splitting an index by its contexts.
-    , _splitByContexts               :: Int -> [Index]
+    , _splitByContexts               :: Int -> [Index i]
 
     -- | Splitting an index by its documents.
-    , _splitByDocuments              :: Int -> [Index]
+    , _splitByDocuments              :: Int -> [Index i]
 
     -- | Splitting an index by its words.
-    , _splitByWords                  :: Int -> [Index]
+    , _splitByWords                  :: Int -> [Index i]
 
     -- | Update document id's (e.g. for renaming documents). If the function maps two different id's
     -- to the same new id, the two sets of word positions will be merged if both old id's are present
     -- in the occurrences for a word in a specific context.
-    , _updateDocIds                  :: (Context -> Word -> DocId -> DocId) -> Index
+    , _updateDocIds                  :: (Context -> Word -> DocId -> DocId) -> Index i
 
     -- | Update document id's with a simple injective editing function.
-    , _updateDocIds'                 :: (DocId -> DocId) -> Index
+    , _updateDocIds'                 :: (DocId -> DocId) -> Index i
     --updateDocIds' f               = updateDocIds (const . const $ f)
 
     -- Convert an Index to a list. Can be used for easy conversion between different index
     -- implementations
-
     , _toList                        :: [(Context, Word, Occurrences)]
 
     -- Create an Index from a list. Can be used for easy conversion between different index
@@ -196,6 +199,8 @@ data Index = Ix
 
     --, _fromList                      :: [(Context, Word, Occurrences)] -> Index
     --fromList e                    = foldl (\i (c,w,o) -> insertOccurrences c w o i) e
+
+    , _impl                          :: i
     }
 
 
@@ -216,7 +221,7 @@ type Parts              = Map Context Part
 type Part               = PT.PrefixTree CompressedOccurrences
 
 
-newIndex :: Inverted -> Index
+newIndex :: Inverted -> Index Inverted
 newIndex i =
     Ix
     {
@@ -258,12 +263,11 @@ newIndex i =
     -- | Delete documents completely (all occurrences).
     , _deleteDocsById         = \ds -> newIndex $ deleteDocsById' ds i
 
-    -- FIXME: no way to access the internal structure without exposing it with the Index data type!?
     -- | Merges two indexes.
-    --, _mergeIndexes           = undefined
+    , _mergeIndexes           = \i2 -> newIndex $ mergeIndexes' i (_impl i2)
 
-    -- | Substract one index from another.
-    --, _substractIndexes       = undefined -- s.o.
+    -- | Subtract one index from another.
+    , _subtractIndexes       = \i2 -> newIndex $ subtractIndexes' i (_impl i2)
 
     -- | Splitting an index by its contexts.
     , _splitByContexts        = map newIndex . splitByContexts' i
@@ -285,12 +289,11 @@ newIndex i =
 
     -- Convert an Index to a list. Can be used for easy conversion between different index
     -- implementations
-
     , _toList                 = undefined
 }
 
 
-emptyIndex                        :: Index
+emptyIndex                        :: Index Inverted
 emptyIndex                        = newIndex (Inverted M.empty)
 
 -- ----------------------------------------------------------------------------
@@ -317,17 +320,17 @@ mergePart                         = PT.unionWith mergeDiffLists
   mergeDiffLists o1 o2            = deflateOcc $
                                   mergeOccurrences (inflateOcc o1) (inflateOcc o2)
 
--- | Substract a set of index parts from another.
-substractParts                    :: Parts -> Parts -> Parts
-substractParts                    = M.differenceWith substractPart
+-- | Subtract a set of index parts from another.
+subtractParts                     :: Parts -> Parts -> Parts
+subtractParts                     = M.differenceWith subtractPart
 
--- | Substract one index part from another.
-substractPart                     :: Part -> Part -> Maybe Part
-substractPart p1 p2               = if PT.null diffPart then Nothing else Just diffPart
+-- | Subtract one index part from another.
+subtractPart                      :: Part -> Part -> Maybe Part
+subtractPart p1 p2                = if PT.null diffPart then Nothing else Just diffPart
   where
-  diffPart                        = PT.differenceWith substractDiffLists p1 p2
+  diffPart                        = PT.differenceWith subtractDiffLists p1 p2
     where
-    substractDiffLists o1 o2      = if diffOcc == emptyOccurrences then Nothing else Just (deflateOcc diffOcc)
+    subtractDiffLists o1 o2      = if diffOcc == emptyOccurrences then Nothing else Just (deflateOcc diffOcc)
       where
       diffOcc                     = substractOccurrences (inflateOcc o1) (inflateOcc o2)
 
@@ -380,14 +383,14 @@ lookupNoCase' i c q               = map (T.pack *** inflateOcc)  $ PT.lookupNoCa
 mergeIndexes'                     :: Inverted -> Inverted -> Inverted
 mergeIndexes' i1 i2               = Inverted (mergeParts (indexParts i1) (indexParts i2))
 
-substractIndexes'                 :: Inverted -> Inverted -> Inverted
-substractIndexes' i1 i2           = Inverted (substractParts (indexParts i1) (indexParts i2))
+subtractIndexes'                  :: Inverted -> Inverted -> Inverted
+subtractIndexes' i1 i2            = Inverted (subtractParts (indexParts i1) (indexParts i2))
 
 insertOccurrences'                :: Context -> Text -> Occurrences -> Inverted -> Inverted
 insertOccurrences' c w o          = mergeIndexes' (singleton c w o)
 
 deleteOccurrences'                :: Context -> Text -> Occurrences -> Inverted -> Inverted
-deleteOccurrences' c w o i        = substractIndexes' i (singleton c w o)
+deleteOccurrences' c w o i        = subtractIndexes' i (singleton c w o)
 
 splitByContexts'                  :: Inverted -> Int -> [Inverted]
 splitByContexts' (Inverted ps)    = splitInternal (map (uncurry annotate) . M.toList $ ps)
