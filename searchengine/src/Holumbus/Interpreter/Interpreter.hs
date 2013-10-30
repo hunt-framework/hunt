78ef2ea41debfcc117b394c854adecbd2710f73b
Ulf Sauer <ulf.sauer@gmail.com>
2013-10-30 17:33:48 +0100

    Implement completion and refactor search

--- searchengine/src/Holumbus/Interpreter/Interpreter.hs
+++ searchengine/src/Holumbus/Interpreter/Interpreter.hs
@@ -8,16 +8,18 @@ import           Control.Monad.Error
 import           Control.Monad.Reader
 
 import qualified Data.Binary                       as Bin
+import qualified Data.Map                          as M
 import           Data.Set                          (Set)
 import           Data.Text                         (Text)
 import qualified Data.Text                         as T
 
 
-import           Holumbus.Common.BasicTypes
 import           Holumbus.Common.ApiDocument       as ApiDoc
-import           Holumbus.Common.Occurrences       (Occurrences)
-import           Holumbus.Common.Document          (Document, unwrap)
+import           Holumbus.Common.BasicTypes
 import qualified Holumbus.Common.DocIdMap          as DM
+import           Holumbus.Common.Document          (Document, DocumentWrapper,
+                                                    unwrap)
+import           Holumbus.Common.Occurrences       (Occurrences)
 
 import           Holumbus.Analyzer.Analyzer
 
@@ -38,6 +40,7 @@ import qualified Holumbus.DocTable.DocTable        as Dt
 import           Holumbus.DocTable.HashedDocuments as HDt
 
 import           Holumbus.Interpreter.Command
+
 -- ----------------------------------------------------------------------------
 --
 -- the semantic domains (datatypes for interpretation)
@@ -151,12 +154,10 @@ throwNYI c = throwResError 501 $ "command not yet implemented: " `T.append` (T.p
 
 execCmd :: Command -> CM CmdResult
 execCmd (Search q)
-    = withIx $ case q of
-        Right qry -> execSearch $ qry
-        Left  str ->
-          case parseQuery (T.unpack str) of
-            Right qry -> execSearch $ qry
-            Left  err -> const $ throwResError 500 err
+    = withIx $ execSearch' wrapSearch q
+
+execCmd (Completion s)
+    = withIx $ execSearch' wrapCompletion (Left s)
 
 execCmd (Sequence cs)
     = execSequence cs
@@ -176,16 +177,7 @@ execCmd (StoreIx filename)
 execCmd (LoadIx filename)
     = modIx $ \_ix -> execLoad filename
 
-execCmd c
-    = throwNYI $ show c
-
 -- ----------------------------------------------------------------------------
-
-execSearch :: Query -> IpIndexer -> CM CmdResult
-execSearch q (ix, dt) = do
-    res <- runQueryM ix dt q
-    return $ ResSearch $ map (\(_, (DocInfo d _, _)) -> unwrap d) . DM.toList . docHits $ res
-
 
 execSequence :: [Command] -> CM CmdResult
 execSequence []       = execCmd NOOP
@@ -202,6 +194,35 @@ execInsert doc op ixx = do
     case op of
         New     -> return (ix', ResOK) -- TODO: not the real deal yet
         x       -> throwNYI $ show x
+
+
+execSearch' :: (Result Document -> CmdResult)
+            -> Either Text Query
+            -> IpIndexer
+            -> CM CmdResult
+execSearch' f q (ix, dt)
+    = case q of
+        Right qry -> runQ qry
+        Left  str ->
+          case parseQuery (T.unpack str) of
+            Right qry -> runQ qry
+            Left  err -> throwResError 500 err
+    where runQ qry = runQueryM ix dt qry >>= return . f
+
+wrapSearch :: Result Document -> CmdResult
+wrapSearch
+    = ResSearch
+      . map (\(_, (DocInfo d _, _)) -> unwrap d)
+      . DM.toList
+      . docHits
+
+wrapCompletion :: Result e -> CmdResult
+wrapCompletion
+    = ResCompletion
+      . map fst -- delete line to get the number of occurrences
+      . map (\(c, (_, o)) -> (c, M.foldr (\m r -> r + DM.size m) 0 o))
+      . M.toList
+      . wordHits
 
 
 execDelete :: Set URI -> IpIndexer -> CM (IpIndexer, CmdResult)
