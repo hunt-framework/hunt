6f30b71de046dc349bc3e9c16abeffd6fcf1d4cb
Ulf Sauer <ulf.sauer@gmail.com>
2013-11-21 21:20:14 +0100

    Comments and cleanup
    - lengthy comment about schema interpretation for insert and search
    - QuickCheck dependency removed, which was added by mistake with
      commit 38d86c638bf0138351c3a858e5700465011f6c1b

--- searchengine/src/Holumbus/Common/Schema.hs
+++ searchengine/src/Holumbus/Common/Schema.hs
@@ -12,18 +12,45 @@ import Holumbus.Common.BasicTypes
 -- ----------------------------------------------------------------------------
 
 -- | Schema
-type ContextSchema = Map Context ContextType
+type ContextSchema
+  = Map Context ContextType
 
-type ContextType   = (CType, CRegex, [CNormalizer], CWeight)
+-- | The type can be any of the supported basic index types.
+--   The regexp validates and splits the text into words:
+--     []  -> invalid
+--     xs  -> words/tokens
+--   Every type can have a type-specific regexp.
+--     => This means is has to match both the type-specific and the context-specific regexp.
+--        Example: A CDate text has to match the type-specific regexp (XMLSchema-Date)
+--                 (requirement for the corresponding Date-Parser which is used to normalize)
+--   Every type can have a type-specific normalizer.
+--     => This means it is first transformed by the type-specific normalizer and then by the
+--        context-specific normalizers
+--
+--   /TL;DR/
+--   Every input for both search and insert has
+--     - two regexps    for validation and tokenization
+--     - two normalizer for transformation
+--   The first  regexp/normalizer is type-specific and is applied first (forced)
+--   The second regexp/normalizer is context-specific (defined/chosen by user)
+type ContextType
+  = (CType, CRegex, [CNormalizer], CWeight)
 
-type CRegex = Text
-type CWeight = Float
-
-data CType  = CText | CInt
+-- | Types for values in a context.
+data CType
+  = CText
+  | CInt
   deriving (Show, Eq)
 
+-- | Regular expression.
+type CRegex  = Text
+
+-- | Enum for text-normalizers than can be chose by the user.
 data CNormalizer = CUpperCase | CLowerCase
   deriving (Show, Eq)
+
+-- | Context weight for search result rankings.
+type CWeight = Float
 
 -- ----------------------------------------------------------------------------
 -- JSON instances
