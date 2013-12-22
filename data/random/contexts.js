0de2502b42f7b149c9104f48bdf27f0588c0852e
Chris Reumann <chrisreumann@googlemail.com>
2013-12-22 19:07:26 +0100

    context search is validated now so date and geo contexts should not be default anymore

--- data/random/contexts.js
+++ data/random/contexts.js
@@ -40,7 +40,7 @@
     "context": "contextdate",
     "schema": {
         "weight": 1.0,
-        "default": true,
+        "default": false,
         "normalizers": [],
 	"regexp": "[0-9]{4}-((0[1-9])|(1[0-2]))-((0[1-9])|([12][0-9])|(3[01]))",
         "type": "date"
@@ -52,7 +52,7 @@
     "context": "contextgeo",
     "schema": {
         "weight": 1.0,
-        "default": true,
+        "default": false,
         "normalizers": [],
         "regexp": "\\w*",
         "type": "position"
