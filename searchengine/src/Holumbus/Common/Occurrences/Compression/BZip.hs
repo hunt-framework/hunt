8d4432bcea06f9ed043827241ff0bdfb4830e4c7
Ulf Sauer <ulf.sauer@gmail.com>
2013-12-24 18:15:20 +0100

    Remove unnecessary ByteString newtype in Occurrences.Compression

--- searchengine/src/Holumbus/Common/Occurrences/Compression/BZip.hs
+++ searchengine/src/Holumbus/Common/Occurrences/Compression/BZip.hs
@@ -25,7 +25,7 @@ import           Control.DeepSeq
 
 import           Data.Binary                             (Binary)
 import qualified Data.Binary                             as B
-import qualified Data.ByteString.Lazy                    as BL
+import           Data.ByteString.Lazy                    (ByteString)
 
 import qualified Codec.Compression.BZip                  as BZ
 
@@ -45,17 +45,9 @@ mkOccOBs b = OccOBs $! b
 
 -- ----------------------------------------------------------------------------
 
-newtype ByteString = Bs { unBs :: BL.ByteString }
-  deriving (Eq, Show)
-
-mkBs :: BL.ByteString -> ByteString
-mkBs s = Bs $!! s
-
--- ----------------------------------------------------------------------------
-
 instance OccCompression CompressedOccurrences where
-  compressOcc           = mkOccOBs . mkBs . BZ.compress . B.encode
-  decompressOcc         = B.decode . BZ.decompress . unBs . unOccOBs
+  compressOcc           = mkOccOBs . BZ.compress . B.encode
+  decompressOcc         = B.decode . BZ.decompress . unOccOBs
   differenceWithKeySet  = undefined
 
 -- ----------------------------------------------------------------------------
@@ -63,16 +55,5 @@ instance OccCompression CompressedOccurrences where
 instance Binary OccOSerialized where
   put = B.put . unOccOBs
   get = B.get >>= return . OccOBs
-
--- ----------------------------------------------------------------------------
-
-instance NFData ByteString where
--- use default implementation: eval to WHNF, and that's sufficient
-
--- ----------------------------------------------------------------------------
-
-instance Binary ByteString where
-   put = B.put . unBs
-   get = B.get >>= return . mkBs
 
 -- ----------------------------------------------------------------------------
--- searchengine/src/Holumbus/Common/Occurrences/Compression/Snappy.hs
+++ searchengine/src/Holumbus/Common/Occurrences/Compression/Snappy.hs
@@ -30,7 +30,7 @@ import           Control.DeepSeq
 
 import           Data.Binary                             (Binary)
 import qualified Data.Binary                             as B
-import qualified Data.ByteString.Lazy                    as BL
+import           Data.ByteString.Lazy                    (ByteString)
 
 import qualified Codec.Compression.Snappy.Lazy           as BZ
 
@@ -50,17 +50,9 @@ mkOccOBs b = OccOBs $! b
 
 -- ----------------------------------------------------------------------------
 
-newtype ByteString = Bs { unBs :: BL.ByteString }
-  deriving (Eq, Show)
-
-mkBs :: BL.ByteString -> ByteString
-mkBs s = Bs $!! s
-
--- ----------------------------------------------------------------------------
-
 instance OccCompression CompressedOccurrences where
-  compressOcc           = mkOccOBs . mkBs . BZ.compress . B.encode
-  decompressOcc         = B.decode . BZ.decompress . unBs . unOccOBs
+  compressOcc           = mkOccOBs . BZ.compress . B.encode
+  decompressOcc         = B.decode . BZ.decompress . unOccOBs
   differenceWithKeySet  = undefined
 
 -- ----------------------------------------------------------------------------
@@ -68,16 +60,5 @@ instance OccCompression CompressedOccurrences where
 instance Binary OccOSerialized where
   put = B.put . unOccOBs
   get = B.get >>= return . OccOBs
-
--- ----------------------------------------------------------------------------
-
-instance NFData ByteString where
--- use default implementation: eval to WHNF, and that's sufficient
-
--- ----------------------------------------------------------------------------
-
-instance Binary ByteString where
-   put = B.put . unBs
-   get = B.get >>= return . mkBs
 
 -- ----------------------------------------------------------------------------
