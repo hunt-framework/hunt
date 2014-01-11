a7a6d9256f79082b9d98701447df590771860036
Sebastian <sebastian@spawnhost.de>
2014-01-11 19:04:38 +0100

    improved json quality

--- geoFrontend/src/Holumbus/GeoFrondend/Feeder.hs
+++ geoFrontend/src/Holumbus/GeoFrondend/Feeder.hs
@@ -11,6 +11,8 @@ import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Lens (over, mapped, both)
 
 import Data.Maybe (isJust, fromJust)
+import Data.List (intersect)
+import qualified Data.Set as Set
 import Data.Text (Text, pack)
 import qualified Data.ByteString.Char8 as B (unpack)
 import qualified Data.ByteString.Lazy as BL
@@ -33,7 +35,7 @@ makePrisms ''OSM
 writeJSON :: MonadIO m => FilePath -> m ()
 writeJSON p = do
     nwrs <- readXML p
-    let docs = map nwrToGeoDocument nwrs
+    let docs = Set.fromList $ map nwrToGeoDocument nwrs
     liftIO $ writeFile "map.json" (B.unpack $ BL.toStrict $ encode docs)
 
 
@@ -45,8 +47,20 @@ readXML p = do
     let namedNwrs = filter nwrHasName nwrs
     return $ filter (\n -> (isJust $ asNode n) || (isJust $ asWay n)) namedNwrs
 
+supportedTags :: [Tag] -> Bool
+supportedTags tags' =  not . null $ ["amenity", "highway", "historic", "shop", "tourism", "leisure", "website", "wikipedia"] `intersect` (fst $ unzip $ map tagToPair tags')
+
+hasNameTag :: [Tag] -> Bool
+hasNameTag tags' = not . null $ filter (("name" == ) . tagGetKey) tags'
+
+noStopWords :: [Tag] -> Bool
+noStopWords tags' = not $ or $ map (\x -> x `elem` tags') [tag "amenity" "toilets", tag "highway" "bus_stop"]
+
+
 nwrHasName :: NodeWayRelation -> Bool
-nwrHasName nwr = not . null $ filter (("name" == ) . tagGetKey) $ nwrCommonGetTags $ getCommon nwr 
+nwrHasName nwr =  hasNameTag tags' && supportedTags tags' && noStopWords tags'
+    where
+        tags' = nwrCommonGetTags $ getCommon nwr 
 
 getCommon :: NodeWayRelation -> NWRCommon
 getCommon nwr
@@ -62,7 +76,7 @@ data GeoDocument = GeoDocument {
     lat  :: Text,
     kind :: OSMType,
     tags :: [(Text, Text)]
-} deriving (Eq)
+} deriving (Eq, Ord)
 
 instance ToJSON GeoDocument where
     toJSON d = object $ [ "name" .= name d,
@@ -72,7 +86,7 @@ instance ToJSON GeoDocument where
                         ] ++ (map (uncurry (.=)) $ tags d)
 
 data OSMType = Way | Node
-    deriving (Show, Eq, Generic)
+    deriving (Show, Eq, Generic, Ord)
 
 instance ToJSON OSMType
 
@@ -80,7 +94,7 @@ nwrToGeoDocument :: NodeWayRelation -> GeoDocument
 nwrToGeoDocument nwr = GeoDocument nameTag nwrLon nwrLat kind' (filter (\(k,_) -> k /= "name") tags')
     where
         tags' :: [(Text,Text)]
-        tags' = over (mapped.both) pack $ fmap tagToPair $ nwrCommonGetTags $ getCommon nwr
+        tags' = over (mapped.both) pack $ filterIndexedTags $ fmap tagToPair $ nwrCommonGetTags $ getCommon nwr
         nameTag :: Text
         nameTag = fromJust $ lookup "name" tags'
         (nwrLon, nwrLat) = over both pack $ nwrGetCoords nwr
@@ -91,3 +105,26 @@ nwrGetCoords nwr = foldNodeWayRelation nwr fNode fWay undefined
     where
         fNode n = (nodeGetLon n, nodeGetLat n)
         fWay _ = ("", "")
+
+indexedTags :: [String]
+indexedTags = [
+      "name"
+    , "amenity"
+    , "highway"
+    , "historic"
+    , "shop"
+    , "tourism"
+    , "leisure"
+    , "website"
+    , "wikipedia"
+
+    , "place"
+    , "memorial:type"
+    , "contact:website"
+    , "cuisine"
+    , "operator"
+    , "phone"
+    , "fax"]
+
+filterIndexedTags :: [(String, String)] -> [(String, String)]
+filterIndexedTags tags' = filter (\(k,_) -> k `elem` indexedTags) tags'
