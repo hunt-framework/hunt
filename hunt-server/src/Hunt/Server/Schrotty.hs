0614ea1b4ae5568a7c509b0f15f38462fc1d21c9
Ulf Sauer <ulf.sauer@gmail.com>
2014-02-09 21:54:21 +0100

    Use new ScottyT custom exception type capability

--- hunt-server/src/Hunt/Server/Schrotty.hs
+++ hunt-server/src/Hunt/Server/Schrotty.hs
@@ -3,9 +3,11 @@
 
 module Hunt.Server.Schrotty
 ( module Web.Scotty.Trans
+, ActionSchrotty
+, ActionSchrottyT
 , schrotty
 , schrottyOpts
-, throw
+, handleCustomError
 , jsonData
 , param
 , jsonPretty
@@ -13,24 +15,15 @@ module Hunt.Server.Schrotty
 )
 where
 
-import           Control.Applicative
-import           Control.Monad.Error
-
 import           Network.HTTP.Types
-import           Network.Wai
 import           Network.Wai.Handler.Warp   (Port)
 
-import           Web.Scotty                 (ActionM)
 import           Web.Scotty.Trans           hiding (jsonData, param)
 import qualified Web.Scotty.Trans           as Scotty
---import           Web.Scotty.Util
 
-import           Data.ByteString.Lazy       (ByteString)
-import qualified Data.ByteString.Lazy.Char8 as BSL
+import           Data.Text.Lazy             (Text)
 import qualified Data.Text.Lazy             as TL
-import qualified Data.Text.Lazy.Encoding    as TEnc
 
---import           Data.Aeson                   (FromJSON, ToJSON)
 import           Data.Aeson                 as A
 import           Data.Aeson.Encode.Pretty   as AP
 
@@ -39,94 +32,78 @@ import           Hunt.Server.Common
 
 -- ----------------------------------------------------------------------------
 
-newtype WebErrorM a = WebErrorM { runWebErrorM :: ErrorT WebError IO a }
-  deriving (Functor, Applicative, Monad, MonadIO, MonadError WebError)
+type ActionSchrottyT m a = ActionT WebError m a
 
--- TODO: use ScottyT error stack...
-type Schrotty = ScottyT WebError WebErrorM
+type ActionSchrotty a    = ActionT WebError IO a
 
 data WebError
   = InterpreterError CmdError
   | NotFound
   | JsonInvalid      String
-  | MissingParam     ByteString
-  | Other            ByteString
-  | Text             Int ByteString
+  | MissingParam     Text
+  | Other            Text
+  | Text             Int Text
   | forall a . ToJSON a =>
     Json             Int a
 -- XXX: no Show instance because it's not a Haskell98 type
 
-instance Error WebError where
-  strMsg = Other . BSL.pack
-
 instance ScottyError WebError where
-  stringError = Text 500 . BSL.pack
-  showError _ = "schrotty exception"
+  stringError = Text 500 . TL.pack
+  showError _ = "schrotty exception :("
 
 -- ----------------------------------------------------------------------------
 
-handleCustomError :: Monad m => Int -> ByteString -> m Response
-handleCustomError code bs = return $ plainResponse (toEnum code) bs
-
-
-handleError :: WebError -> IO Response
-handleError (NotFound)
-  = return $ plainResponse status404 $ "Not found."
-handleError (JsonInvalid s)
-  = return $ jsonResponse status400 $ JsonFailure 400 ("json invalid: " ++ s)
-handleError (MissingParam bs)
-  = return $ plainResponse status404 $ "Query parameter missing " `BSL.append` bs
-handleError (Other bs)
-  = return $ plainResponse status500 bs
-handleError (Text code bs)
-  = handleCustomError code bs
-handleError (Json code o)
-  = return $ jsonResponse (toEnum code) (JsonFailure code o)
-handleError (InterpreterError (ResError code msg))
-  = handleError (Json code msg)
-  -- = handleCustomError code $ TEnc.encodeUtf8 . TL.fromStrict $ msg
-
-
-plainResponse :: Status -> ByteString -> Response
-plainResponse st bs = responseLBS st [("Content-type","text/plain; charset=utf-8")] bs
-
-jsonResponse :: ToJSON a => Status -> a -> Response
-jsonResponse st js = responseLBS st [("Content-type","application/json")] (A.encode js)
-
-throw :: MonadTrans t => WebError -> t WebErrorM a
-throw = lift . throwError
+handleCustomError :: Monad m => WebError -> ActionSchrottyT m ()
+handleCustomError (NotFound)
+  = do
+    status status404
+    text $ "Not found."
+handleCustomError (JsonInvalid s)
+  = do
+    status status400
+    jsonPretty $ JsonFailure 400 ("json invalid: " ++ s)
+handleCustomError (MissingParam txt)
+  = do
+    status status404
+    text $ "Query parameter missing " `TL.append` txt -- TODO: fix types
+handleCustomError (Other txt)
+  = do
+    status status500
+    text $ txt
+handleCustomError (Text code bs)
+  = do
+    status (toEnum code)
+    text $ bs
+handleCustomError (Json code o)
+  = do
+    status (toEnum code)
+    jsonPretty $ JsonFailure code o
+handleCustomError (InterpreterError (ResError code msg))
+  = handleCustomError (Json code msg)
 
 -- | Like 'Web.Scotty.json', but pretty.
-jsonPretty :: ToJSON a => a -> ActionM ()
+jsonPretty :: (ToJSON a, ScottyError e, Monad m) => a -> ActionT e m ()
 jsonPretty v = do
   setHeader "Content-Type" "application/json"
   raw $ AP.encodePretty v
 
 -- | Replacement for 'Web.Scotty.jsonData' with custom error.
-jsonData :: (FromJSON a, ScottyError e) => ActionT e WebErrorM a
+jsonData :: (FromJSON a, Monad m) => ActionSchrottyT m a
 jsonData = do
   b <- body
   case A.eitherDecode b of
-    (Right j) -> return j
-    (Left e)  -> throw (JsonInvalid e)
+    Right j -> return j
+    Left e  -> raise $ JsonInvalid e
 
 -- | Replacement for 'Web.Scotty.param' with custom error.
-param :: (Parsable a, ScottyError e) => TL.Text -> ActionT e WebErrorM a
+param :: (Parsable a, Monad m) => TL.Text -> ActionSchrottyT m a
 param p = Scotty.param p
-            `rescue` (\_ -> throw $ MissingParam $ TEnc.encodeUtf8 p)
+            `rescue` (\_ -> raise $ MissingParam p)
 
 -- ----------------------------------------------------------------------------
-runM :: WebErrorM b -> IO b
-runM m = do
-  r <- runErrorT (runWebErrorM m)
-  either (\_ex -> fail $ "exception at startup") return r
 
--- | 'runActionToIO' is called once per action
-runActionToIO :: WebErrorM Response -> IO Response
-runActionToIO m = runErrorT (runWebErrorM m) >>= either handleError return
+schrotty :: Port -> ScottyT WebError IO () -> IO ()
+schrotty p a = Scotty.scottyT p id id $ defaultHandler handleCustomError >> a
 
-schrotty :: Port -> Schrotty () -> IO ()
-schrotty p = Scotty.scottyT p runM runActionToIO
-
-schrottyOpts :: Options -> Schrotty () -> IO ()
-schrottyOpts o = Scotty.scottyOptsT o runM runActionToIO
+schrottyOpts :: Options -> ScottyT WebError IO () -> IO ()
+schrottyOpts o a = Scotty.scottyOptsT o id id $ defaultHandler handleCustomError >> a
