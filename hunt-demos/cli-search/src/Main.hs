bae1a97949ff16cad9468726f359c7042948ebdc
Chris Reumann <chrisreumann@googlemail.com>
2014-02-11 14:41:15 +0100

    cli search example finished

--- hunt-demos/cli-search/src/Main.hs
+++ hunt-demos/cli-search/src/Main.hs
@@ -1,13 +1,11 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
-import           Data.Aeson                     (toJSON, eitherDecode)
-import           Data.Aeson.Encode.Pretty
 import           Data.Text                      (Text, unpack)
-import           Data.Map                       hiding (map)
-import qualified Data.ByteString.Lazy           as B
+import           Data.Time.Clock
 
 import           Hunt.Common
+import           Hunt.Common.ApiDocument
 import           Hunt.Index.Schema
 import           Hunt.Interpreter.Interpreter
 import           Hunt.Interpreter.Command
@@ -15,44 +13,58 @@ import           Hunt.Query.Ranking
 import           Hunt.Query.Language.Grammar
 import           Hunt.Query.Language.Parser
 
-import           System.Console.CmdArgs (cmdArgs, (&=), explicit, name, help, summary)
-import           Data.Typeable (Typeable)
-import           Data.Data (Data)
+import           System.Environment   
 
-data CliConfig = CliConfig {
-    cfgIndex :: String
-} deriving (Show, Data, Typeable)
-
-
-mkConfig :: CliConfig
-mkConfig = CliConfig {
-        cfgIndex = "index"  &= name "index" &= help "Path to a file containing a binary serialized hunt index."
-    } &= summary "Hunt command line search"
-
+-- | Hunt command line search.
+--   After index is loaded, queries can be performed
+--   'exit' stops the application
 main :: IO ()  
 main = do
-    config <- cmdArgs mkConfig
+    args <- getArgs
+   
+    putStrLn $ "loading index from file " ++ head args
     ix <- initEnv emptyIndexer defaultRankConfig contextTypes
-    putStrLn $ "loading index from file " ++ cfgIndex config
-    res <- runCmd ix $ LoadIx (cfgIndex config)
-    _ <-case res of 
-         (Right _)  -> return ()
-         (Left err) -> error $ show err
-    processQuery ix
-    return ()
+    res <- runCmd ix $ LoadIx (head args)
+  
+    case res of 
+       (Left err) -> error $ show err
+       _          -> getQuery ix
+
+    where
+    -- reads query from line
+    getQuery ix = do
+      putStrLn "======================================"
+      putStrLn "Enter query or 'exit':"
+      qry <- getLine
+      if qry == "exit" 
+        then return ()
+        else do
+          case parseQuery qry of
+            (Right query) -> do
+              putStrLn "searching ..."
+              -- no paging here. fetch all results. might lead to slow queries!
+              start <- getCurrentTime
+              res <- runCmd ix $ Search query 0 1000000000000
+              end <- getCurrentTime
+              printQueryResult res (diffUTCTime end start)
+              getQuery ix
+            (Left err)   -> putStrLn $ "Invalid input: " ++ unpack err
+
+-- | Print interpreter search results.
+printQueryResult :: Show t => Either CmdError CmdResult -> t -> IO ()
+printQueryResult res t
+    = case res of 
+        (Left err)  -> do
+            putStrLn $ "Query executing error:" ++ show err
+        (Right r) -> do
+            putStrLn "======================================"
+            putStrLn $ "Query time: " ++ show t
+            putStrLn $ "Query results: " ++ (show . lrCount . crRes $ r)
+            putStr $ "Found uris: "
+            mapM (\(Document uri _) -> putStr $ " " ++ unpack uri) (lrResult . crRes $ r)
+            putStrLn ""
+            return ()         
 
 
-processQuery ix = do
-  putStrLn "Enter query:"
-  qry <- getLine
-  if qry == "exit" 
-    then return()
-    else do
-      case parseQuery qry of
-        (Right query) -> do
-           putStrLn "searching ..."
-           result <- runCmd ix $ Search query 0 10
-           print result
-           return ()
-        (Left err)   -> putStrLn $ "Invalid input: " ++ unpack err
-      processQuery ix
+
+                     
