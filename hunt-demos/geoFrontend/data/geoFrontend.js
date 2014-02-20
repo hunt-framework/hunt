baef50a88b4421d3927ef577ddd9aa05c9c86000
Sebastian <sebastian@spawnhost.de>
2014-02-20 23:18:28 +0100

    added position range of the current map bounds

--- hunt-demos/geoFrontend/data/geoFrontend.js
+++ hunt-demos/geoFrontend/data/geoFrontend.js
@@ -24,10 +24,10 @@ function initMap() {
     // create the tile layer with correct attribution
     var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var osmAttrib='Map data © OpenStreetMap contributors';
-    var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});        
+    var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});        
 
     // start the map in South-East England
-    map.setView(new L.LatLng((53.5681 + 53.5854) / 2, (9.6960 + 9.7496) / 2), 12);
+    map.setView(new L.LatLng((53.5681 + 53.5854) / 2, (9.6960 + 9.7496) / 2), 14);
     map.addLayer(osm);
 }
 
@@ -39,7 +39,12 @@ function onSearch(){
     })  
     markers = []
 
-    var data = {term: query}
+    bounds = map.getBounds()
+    ne = bounds.getNorthEast()
+    sw = bounds.getSouthWest()
+    boundsString = " position:[" + ne.lat + "-" + ne.lng + " TO " + sw.lat + "-" + sw.lng + "]" 
+
+    var data = {term: query + boundsString}
     $.getJSON( "search", data, function( data ) {
         $.each(data, function(i, doc){
             var lat = doc.lat
