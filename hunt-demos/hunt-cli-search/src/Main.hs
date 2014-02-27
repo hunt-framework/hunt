57949ae78c4673bd56156ad8ee9a26a81ae82df9
Ulf Sauer <ulf.sauer@gmail.com>
2014-02-27 14:49:15 +0100

    Cleanup

--- hunt-demos/hunt-cli-search/src/Main.hs
+++ hunt-demos/hunt-cli-search/src/Main.hs
@@ -13,22 +13,22 @@ import           Hunt.Query.Ranking
 import           Hunt.Query.Language.Grammar
 import           Hunt.Query.Language.Parser
 
-import           System.Environment   
+import           System.Environment
 
 -- | Hunt command line search.
 --   After index is loaded, queries can be performed
 --   'exit' stops the application
-main :: IO ()  
+main :: IO ()
 main = do
     args <- getArgs
-   
+
     putStrLn $ "loading index from file " ++ head args
-    ix <- initEnv emptyIndexer defaultRankConfig contextTypes
+    ix  <- initEnv emptyIndexer defaultRankConfig contextTypes
     res <- runCmd ix $ LoadIx (head args)
-  
-    case res of 
-       (Left err) -> error $ show err
-       _          -> getQuery ix
+
+    case res of
+       Left err -> error $ show err
+       _        -> getQuery ix
 
     where
     -- reads query from line
@@ -36,11 +36,11 @@ main = do
       putStrLn "======================================"
       putStrLn "Enter query or 'exit':"
       qry <- getLine
-      if qry == "exit" 
+      if qry == "exit"
         then return ()
         else do
           case parseQuery qry of
-            (Right query) -> do
+            Right query -> do
               putStrLn "searching ..."
               -- no paging here. fetch all results. might lead to slow queries!
               start <- getCurrentTime
@@ -48,23 +48,19 @@ main = do
               end <- getCurrentTime
               printQueryResult res (diffUTCTime end start)
               getQuery ix
-            (Left err)   -> putStrLn $ "Invalid input: " ++ unpack err
+            Left err   -> putStrLn $ "Invalid input: " ++ unpack err
 
 -- | Print interpreter search results.
 printQueryResult :: Show t => Either CmdError CmdResult -> t -> IO ()
 printQueryResult res t
-    = case res of 
-        (Left err)  -> do
+    = case res of
+        Left err-> do
             putStrLn $ "Query executing error:" ++ show err
-        (Right r) -> do
+        Right r -> do
             putStrLn "======================================"
             putStrLn $ "Query time: " ++ show t
             putStrLn $ "Query results: " ++ (show . lrCount . crRes $ r)
             putStr $ "Found uris: "
             mapM (\(Document uri _) -> putStr $ " " ++ unpack uri) (lrResult . crRes $ r)
             putStrLn ""
-            return ()         
-
-
-
-                     
+            return ()
