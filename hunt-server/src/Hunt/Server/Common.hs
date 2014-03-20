63c0d93b5c6a02fce1c11232e393064ac7e4ad2e
Ulf Sauer <ulf.sauer@gmail.com>
2014-03-20 04:55:50 +0100

    Use Data.Default with Hunt configuration

--- hunt-server/src/Hunt/Server/Common.hs
+++ hunt-server/src/Hunt/Server/Common.hs
@@ -4,12 +4,16 @@
 
 module Hunt.Server.Common where
 
-import           Data.Data         (Data)
-import           Data.Typeable     (Typeable)
+import           Data.Data              (Data)
+import           Data.Default
+import           Data.Typeable          (Typeable)
 
 import           Data.Aeson
 
-import           System.Log.Logger (Priority(..))
+import           System.Console.CmdArgs (explicit, help, name, program,
+                                         summary, typ, typFile, (&=))
+import           System.Log.Logger      (Priority (..))
+
 
 -- ----------------------------------------------------------------------------
 
@@ -30,6 +34,8 @@ instance ToJSON (JsonResponse r f) where
     , "msg"   .= msg
     ]
 
+-- ----------------------------------------------------------------------------
+
 -- instances necessary for System.Console.CmdArgs
 deriving instance Data     Priority
 deriving instance Typeable Priority
@@ -42,3 +48,24 @@ data HuntServerConfiguration = HuntServerConfiguration
   , logFile            :: FilePath        -- ^ The location of the logfile.
   , logPriority        :: Priority        -- ^ The priority level to log on stdout.
   } deriving (Show, Data, Typeable)
+
+
+instance Default HuntServerConfiguration where
+  def = HuntServerConfiguration {
+    huntServerHost = "*"
+      &= explicit &= name "host"
+      &= help "Bind to altenate host (* = any, *4 = any IPv4, *6 = any IPv6)",
+    huntServerPort = (3000::Int)
+      &= explicit &= name "port"
+      &= help "Listen on alternate port",
+    readIndexOnStartup = Nothing
+      &= explicit &= name "load-index" &= typFile
+      &= help "Load index on startup",
+    logPriority = DEBUG
+      &= explicit &= name "log-priority" &= typ "PRIORITY"
+      &= help "Set log level for stdout",
+    logFile = "hunt.log"
+      &= explicit &= name "log-file" &= typFile
+      &= help "Set logfile location"
+  } &= summary "Standalone search server based on the hunt searchengine."
+    &= program "hunt-server"
--- hunt-server/src/Main.hs
+++ hunt-server/src/Main.hs
@@ -1,36 +1,11 @@
 module Main where
 
+import           Data.Default
+
 import           Hunt.Server            (start)
-import           Hunt.Server.Common
-import           System.Log.Logger      (Priority(..))
-import           System.Console.CmdArgs (cmdArgs, explicit, help, name, program,
-                                         summary, typ, typFile, (&=))
-
--- ----------------------------------------------------------------------------
-
-huntConfiguration :: HuntServerConfiguration
-huntConfiguration = HuntServerConfiguration {
-    huntServerHost = "*"
-      &= explicit &= name "host"
-      &= help "Bind to altenate host (* = any, *4 = any IPv4, *6 = any IPv6)",
-    huntServerPort = (3000::Int)
-      &= explicit &= name "port"
-      &= help "Listen on alternate port",
-    readIndexOnStartup = Nothing
-      &= explicit &= name "load-index" &= typFile
-      &= help "Load index on startup",
-    logPriority = DEBUG
-      &= explicit &= name "log-priority" &= typ "PRIORITY"
-      &= help "Set log level for stdout",
-    logFile = "hunt.log"
-      &= explicit &= name "log-file" &= typFile
-      &= help "Set logfile location"
-  } &= summary "Standalone search server based on the hunt searchengine."
-    &= program "hunt-server"
+import           System.Console.CmdArgs (cmdArgs)
 
 -- ----------------------------------------------------------------------------
 
 main :: IO ()
-main = do
-  config <- cmdArgs huntConfiguration
-  start config
+main = cmdArgs def >>= start
