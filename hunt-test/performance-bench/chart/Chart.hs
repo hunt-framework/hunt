b83f71cf2603dfa07786e45cc814643ca0f34cbc
Chris Reumann <chrisreumann@googlemail.com>
2014-03-29 10:59:35 +0100

    small change to diagram generation

--- hunt-test/performance-bench/chart/Chart.hs
+++ hunt-test/performance-bench/chart/Chart.hs
@@ -102,21 +102,28 @@ main = do
             & layout_y_axis .~ (def & laxis_title .~ "Execution time in seconds")
 
   -- query benchmark
-  let query1 = def
-            & layout_title  .~ "Execution times of 1000 queries for 'a'" 
-            & layout_plots  .~ [ plotBars $ barDiagram4 
-                                      (reverse [ "BZIP"
+  
+  let q_bars = [plotBars $ barDiagram4 (reverse [ "BZIP"
                                       , "ByteString"
                                       , "Cache + Binary Tree"
                                       , "Uncompressed" 
-                                      ]) query
-                               ]
+                                      ]) query]
+
+  let q_lines =  [ toPlot $ lineDiagram "BZIP" q_bzip
+                 , toPlot $ lineDiagram "ByteString" q_bs
+                 , toPlot $ lineDiagram "Cache + Binary Tree" q_btree
+                 , toPlot $ lineDiagram "Uncompressed" q_unc
+                 ]
+
+  let query1 = def
+            & layout_title  .~ "Execution times of 1000 queries for 'a'" 
+            & layout_plots  .~ q_lines
             & layout_x_axis .~ (def    & laxis_title .~ "Number of indexed documents"
                                        & laxis_generate .~ (autoIndexAxis $ map toDocs [1..11])
                                )
             & layout_y_axis .~ (def & laxis_title .~ "Execution time in seconds")
 
-  mapM (\(plot,file) -> renderableToFile (FileOptions (640,400) SVG M.empty) (toRenderable plot) file) 
+  mapM (\(plot,file) -> renderableToFile (FileOptions (640,300) SVG M.empty) (toRenderable plot) file) 
     [ (memory1, "memory1.svg")
     , (memory2, "memory2.svg")
     , (time1  , "time1.svg")
@@ -147,11 +154,12 @@ barDiagram4 titles x =
       & plot_bars_values .~ addIndexes x
       & plot_bars_item_styles .~ map mkstyle (cycle defaultColorSeq2)
 
-lineDiagram :: String -> [[(Int, Double)]] -> PlotLines Int Double
+lineDiagram :: String -> [Double] -> PlotLines PlotIndex Double
 lineDiagram title res = 
   def & plot_lines_title  .~ title
       & plot_lines_style  .~ def
-      & plot_lines_values .~ res
+      & plot_lines_values .~ [addIndexes res]
+--      & plot_lines_item_styles .~ map mkstyle (cycle defaultColorSeq2)
 
 bstyle = Just (solidLine 1.0 $ opaque black)
 mkstyle c = (solidFillStyle c, bstyle)
