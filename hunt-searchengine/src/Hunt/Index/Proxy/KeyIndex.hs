hunt-framework/hunt

commit 717abc05917814f143c0a7789ba1534c70716235

Minor refactoring, documentation and cleanup

hunt-searchengine/src/Hunt/Index/Proxy/KeyIndex.hs
@@ -5,8 +5,20 @@
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE UndecidableInstances       #-}
 
+-- ----------------------------------------------------------------------------
+
+{- |
+  Key conversion proxy.
+  Wraps an index to expose the desired key type.
+  The conversion is defined by the 'Bijection' implementation.
+
+  This can be used for simple conversions like @Text@ to @String@ or normalization and compression.
+-}
+
+-- ----------------------------------------------------------------------------
+
 module Hunt.Index.Proxy.KeyIndex
-( KeyProxyIndex(..)
+( KeyProxyIndex (..)
 )
 where
 
@@ -28,18 +40,24 @@ import           Hunt.Common.Occurrences.Compression
 
 -- ------------------------------------------------------------
 
+-- | Key conversion proxy.
+--   @toType@ is the desired/exposed key type, followed by the wrapped index type.
+--   There has to be a corresponding 'Bijection' instance:
+--
+--   >  instance Bijection (IKey impl v) toType where ...
 newtype KeyProxyIndex toType impl cv
-  = KPIx { kpIx :: impl cv }
+  = KeyProxyIx { keyProxyIx :: impl cv }
   deriving (Eq, Show, NFData)
 
-mkKPIx :: impl cv -> KeyProxyIndex toType impl cv
-mkKPIx v = KPIx $! v
+-- | Wrap an index in a key conversion proxy.
+mkKeyProxyIx :: impl cv -> KeyProxyIndex toType impl cv
+mkKeyProxyIx v = KeyProxyIx $! v
 
 -- ------------------------------------------------------------
 
 instance Binary (impl v) => Binary (KeyProxyIndex toType impl v) where
-  put = put . kpIx
-  get = get >>= return . mkKPIx
+  put = put . KeyProxyIx
+  get = get >>= return . mkKeyProxyIx
 
 -- ------------------------------------------------------------
 
@@ -52,40 +70,40 @@ instance Index (KeyProxyIndex toType impl) where
     , Bijection (IKey impl v) toType
     )
 
-  insertList kvs (KPIx i)
-    = mkKPIx $ insertList (P.map (first from) kvs) i
+  insertList kvs (KeyProxyIx i)
+    = mkKeyProxyIx $ insertList (P.map (first from) kvs) i
 
-  deleteDocs ks (KPIx i)
-    = mkKPIx $ deleteDocs ks i
+  deleteDocs ks (KeyProxyIx i)
+    = mkKeyProxyIx $ deleteDocs ks i
 
   empty
-    = mkKPIx $ empty
+    = mkKeyProxyIx $ empty
 
   fromList l
-    = mkKPIx . fromList $ P.map (first from) l
+    = mkKeyProxyIx . fromList $ P.map (first from) l
 
-  toList (KPIx i)
+  toList (KeyProxyIx i)
     = first to <$> toList i
 
-  search t k (KPIx i)
+  search t k (KeyProxyIx i)
     = first to <$> search t (from k) i
 
-  lookupRange k1 k2 (KPIx i)
+  lookupRange k1 k2 (KeyProxyIx i)
     = first to <$> lookupRange (from k1) (from k2) i
 
-  unionWith op (KPIx i1) (KPIx i2)
-    = mkKPIx $ unionWith op i1 i2
+  unionWith op (KeyProxyIx i1) (KeyProxyIx i2)
+    = mkKeyProxyIx $ unionWith op i1 i2
 
-  unionWithConv t f (KPIx i1) (KPIx i2)
-    = mkKPIx $ unionWithConv t f i1 i2
+  unionWithConv t f (KeyProxyIx i1) (KeyProxyIx i2)
+    = mkKeyProxyIx $ unionWithConv t f i1 i2
 
-  map f (KPIx i)
-    = mkKPIx $ Ix.map f i
+  map f (KeyProxyIx i)
+    = mkKeyProxyIx $ Ix.map f i
 
-  mapMaybe f (KPIx i)
-    = mkKPIx $ Ix.mapMaybe f i
+  mapMaybe f (KeyProxyIx i)
+    = mkKeyProxyIx $ Ix.mapMaybe f i
 
-  keys (KPIx i)
+  keys (KeyProxyIx i)
     = P.map to $ keys i
 
 -- special instance for a CompressedOccurrences proxy within a TextKey proxy
@@ -102,41 +120,41 @@ instance Index (KeyProxyIndex toType (ComprOccIndex impl to)) where
     )
 
   -- this is the only "special" function
-  deleteDocs docIds (KPIx (ComprIx pt))
-    = mkKPIx $ mkComprIx $ Ix.map (differenceWithKeySet docIds) pt
+  deleteDocs docIds (KeyProxyIx (ComprIx pt))
+    = mkKeyProxyIx $ mkComprIx $ Ix.map (differenceWithKeySet docIds) pt
 
   -- everything below is copied toType the more general instance Index (KeyProxyIndex impl)
-  insertList kvs (KPIx i)
-    = mkKPIx $ insertList (P.map (first from) kvs) i
+  insertList kvs (KeyProxyIx i)
+    = mkKeyProxyIx $ insertList (P.map (first from) kvs) i
 
   empty
-    = mkKPIx $ empty
+    = mkKeyProxyIx $ empty
 
   fromList l
-    = mkKPIx . fromList $ P.map (first from) l
+    = mkKeyProxyIx . fromList $ P.map (first from) l
 
-  toList (KPIx i)
+  toList (KeyProxyIx i)
     = first to <$> toList i
 
-  search t k (KPIx i)
+  search t k (KeyProxyIx i)
     = first to <$> search t (from k) i
 
-  lookupRange k1 k2 (KPIx i)
+  lookupRange k1 k2 (KeyProxyIx i)
     = first to <$> lookupRange (from k1) (from k2) i
 
-  unionWith op (KPIx i1) (KPIx i2)
-    = mkKPIx $ unionWith op i1 i2
+  unionWith op (KeyProxyIx i1) (KeyProxyIx i2)
+    = mkKeyProxyIx $ unionWith op i1 i2
 
-  unionWithConv t f (KPIx i1) (KPIx i2)
-    = mkKPIx $ unionWithConv t f i1 i2
+  unionWithConv t f (KeyProxyIx i1) (KeyProxyIx i2)
+    = mkKeyProxyIx $ unionWithConv t f i1 i2
 
-  map f (KPIx i)
-    = mkKPIx $ Ix.map f i
+  map f (KeyProxyIx i)
+    = mkKeyProxyIx $ Ix.map f i
 
-  mapMaybe f (KPIx i)
-    = mkKPIx $ Ix.mapMaybe f i
+  mapMaybe f (KeyProxyIx i)
+    = mkKeyProxyIx $ Ix.mapMaybe f i
 
-  keys (KPIx i)
+  keys (KeyProxyIx i)
     = P.map to $ keys i
 
 -- ------------------------------------------------------------

hunt-searchengine/src/Hunt/Common/DocIdMap.hs
@@ -21,42 +21,42 @@
 -- ----------------------------------------------------------------------------
 
 module Hunt.Common.DocIdMap
-    ( DocIdMap(..)
-    , DocIdSet
-    , empty
-    , singleton
-    , null
-    , member
-    , lookup
-    , insert
-    , delete
-    , insertWith
-    , size
-    , minKey
-    , maxKey
-    , isIntervall
-    , union
-    , intersection
-    , difference
-    , diffWithSet
-    , unionWith
-    , intersectionWith
-    , differenceWith
-    , unionsWith
-    , map
-    , filter
-    , filterWithKey
-    , mapWithKey
-    , traverseWithKey
-    , foldr
-    , foldrWithKey
-    , fromList
-    , fromAscList
-    , toList
-    , keys
-    , elems
-    , toDocIdSet
-    )
+  ( DocIdMap(..)
+  , DocIdSet
+  , empty
+  , singleton
+  , null
+  , member
+  , lookup
+  , insert
+  , delete
+  , insertWith
+  , size
+  , minKey
+  , maxKey
+  , isIntervall
+  , union
+  , intersection
+  , difference
+  , diffWithSet
+  , unionWith
+  , intersectionWith
+  , differenceWith
+  , unionsWith
+  , map
+  , filter
+  , filterWithKey
+  , mapWithKey
+  , traverseWithKey
+  , foldr
+  , foldrWithKey
+  , fromList
+  , fromAscList
+  , toList
+  , keys
+  , elems
+  , toDocIdSet
+  )
 where
 
 import           Prelude                    hiding (filter, foldr, lookup, map,
@@ -115,28 +115,29 @@ instance Binary v => Binary (DocIdMap v) where
 -- ------------------------------------------------------------
 
 instance ToJSON v => ToJSON (DocIdMap v) where
-    toJSON = object . L.map toJ . IM.toList . unDIM
+  toJSON = object . L.map toJ . IM.toList . unDIM
+    where
+    toJ (k, v) = (T.pack . toH $ k) .= toJSON v
+    toH !y = "0x" ++ toX 16 "" y
+      where
+      toX :: Int -> String -> Int -> String
+      toX !0 !acc _ = acc
+      toX !n !acc x = toX (n - 1) (d : acc) x'
         where
-          toJ (k, v) = (T.pack . toH $ k) .= toJSON v
-          toH !y = "0x" ++ toX 16 "" y
-              where
-                toX :: Int -> String -> Int -> String
-                toX !0 !acc _ = acc
-                toX !n !acc x = toX (n - 1) (d : acc) x'
-                    where
-                      (!x', !r) = x `divMod` 16
-                      !d | r < 10    = toEnum (fromEnum '0' + r)
-                         | otherwise = toEnum (fromEnum 'a' + r - 10)
+        (!x', !r) = x `divMod` 16
+        !d | r < 10    = toEnum (fromEnum '0' + r)
+           | otherwise = toEnum (fromEnum 'a' + r - 10)
 
 instance FromJSON v => FromJSON (DocIdMap v) where
-    parseJSON (Object o) = DIM <$> foldM parsePair IM.empty (HM.toList o)
-                           where
-                             parsePair res (k, v)
-                                 = case readMaybe . T.unpack $ k of
-                                     Nothing -> mzero
-                                     Just k' -> do v' <- parseJSON v
-                                                   return $ IM.insert k' v' res
-    parseJSON _          = mzero
+  parseJSON (Object o) = DIM <$> foldM parsePair IM.empty (HM.toList o)
+    where
+    parsePair res (k, v)
+      = case readMaybe . T.unpack $ k of
+          Nothing -> mzero
+          Just k' -> do
+            v' <- parseJSON v
+            return $ IM.insert k' v' res
+  parseJSON _          = mzero
 
 -- ------------------------------------------------------------
 

hunt-searchengine/src/Hunt/ContextIndex.hs
@@ -60,7 +60,7 @@ mkContextMap x = ContextMap $! x
 -- ----------------------------------------------------------------------------
 
 getContextMap :: [IndexImpl Occurrences] -> Get (ContextMap Occurrences)
-getContextMap ts = liftM M.fromDistinctAscList (Impl.get' ts) >>= return . ContextMap
+getContextMap ts = liftM M.fromDistinctAscList (Impl.gets' ts) >>= return . ContextMap
 
 instance Binary v => Binary (ContextMap v) where
   put = put . cxMap

hunt-searchengine/src/Hunt/Index/ComprPrefixTreeIndex.hs
@@ -3,8 +3,15 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeFamilies               #-}
 
+-- ----------------------------------------------------------------------------
+{- |
+  Compressed 'StringMap' index.
+  The value has to implement 'OccCompression'.
+-}
+-- ----------------------------------------------------------------------------
+
 module Hunt.Index.ComprPrefixTreeIndex
-( ComprOccPrefixTree(..)
+( ComprOccPrefixTree (..)
 )
 where
 
@@ -33,10 +40,12 @@ import           Hunt.Utility
 
 -- ------------------------------------------------------------
 
+-- | Compressed 'StringMap' index.  The value has to implement 'OccCompression'.
 newtype ComprOccPrefixTree cv
   = ComprPT { comprPT :: SM.StringMap cv}
   deriving (Eq, Show, NFData, Typeable)
 
+-- | Create a compressed 'StringMap' index.
 mkComprPT :: NFData cv => SM.StringMap cv -> ComprOccPrefixTree cv
 mkComprPT cv = ComprPT $! cv
 
@@ -47,6 +56,7 @@ instance (NFData v, Binary v) => Binary (ComprOccPrefixTree v) where
   get = get >>= return . mkComprPT
 
 -- ------------------------------------------------------------
+-- TODO: refactor
 
 instance Index ComprOccPrefixTree where
   type IKey ComprOccPrefixTree v = SM.Key
@@ -66,7 +76,7 @@ instance Index ComprOccPrefixTree where
 
        reduce mapRes
          = case parMap rpar (\(i1,i2) -> unionWith (Occ.merge) i1 i2) $ mkPairs mapRes of
-             []     -> error "this should not be possible..?!?"
+             []     -> error "insertList (ComprOccPrefixTree): impossible case"
              [x]    -> x
              xs     -> reduce xs
 

hunt-searchengine/src/Hunt/Index/ComprPrefixTreeIndex2Dim.hs
@@ -3,8 +3,15 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeFamilies               #-}
 
+-- ----------------------------------------------------------------------------
+{- |
+  Compressed 'StringMap' index for 2-dimensional lookup, e.g. geographic locations.
+  The value has to implement 'OccCompression'.
+-}
+-- ----------------------------------------------------------------------------
+
 module Hunt.Index.ComprPrefixTreeIndex2Dim
-( ComprOccPrefixTree(..)
+( ComprOccPrefixTree (..)
 )
 where
 
@@ -34,10 +41,13 @@ import           Hunt.Utility
 
 -- ------------------------------------------------------------
 
+-- | Compressed 'StringMap' index for 2-dimensional lookup.
+--   The value has to implement 'OccCompression'.
 newtype ComprOccPrefixTree cv
   = ComprPT { comprPT :: SM.StringMap cv}
   deriving (Eq, Show, NFData, Typeable)
 
+-- | Create a compressed 'StringMap' index for 2-dimensional lookup.
 mkComprPT :: NFData cv => SM.StringMap cv -> ComprOccPrefixTree cv
 mkComprPT cv = ComprPT $! cv
 

hunt-searchengine/src/Hunt/Index/IndexImpl.hs
@@ -1,5 +1,20 @@
 {-# LANGUAGE ExistentialQuantification #-}
 
+-- ----------------------------------------------------------------------------
+{- |
+  Heterogeneous indexes using @ExistentialQuantification@.
+
+  Indexes have to implement the 'Index' type class as well as obey other constraints defined in
+  'IndexImplCon'.
+
+  /Note/: Due to the nature of @ExistentialQuantification@, deserialization is tricky because the
+  concrete implementation is not known.
+  This is circumvented by storing the type representation of the index. Deserialization is possible
+  if there is a matching type representation in the set of available types.
+  This requires the use of the custom get functions.
+-}
+-- ----------------------------------------------------------------------------
+
 module Hunt.Index.IndexImpl where
 
 import           Control.Applicative         ((<$>))
@@ -19,6 +34,7 @@ import           Hunt.Index
 
 -- ------------------------------------------------------------
 
+-- | Constraint for index implementations.
 type IndexImplCon i v
   = ( Index i
     , IKey i v ~ Text
@@ -31,6 +47,7 @@ type IndexImplCon i v
 
 -- ------------------------------------------------------------
 
+-- | Index using @ExistentialQuantification@ to allow heterogeneous index containers.
 data IndexImpl v
   = forall i. IndexImplCon i v => IndexImpl { ixImpl :: i v }
 
@@ -51,21 +68,26 @@ instance Binary (IndexImpl v) where
   put (IndexImpl i) = put (typeOf i) >> put i
   get = error "existential types cannot be deserialized this way. Use special get' functions"
 
-get' :: [IndexImpl Occurrences] -> Get [(Context, IndexImpl Occurrences)]
-get' ts = do
-  n <- get :: Get Int
-  getMany' ts n
+-- TODO: refactor
 
-getMany' :: [IndexImpl Occurrences] -> Int -> Get [(Context, IndexImpl Occurrences)]
-getMany' ts n = go [] n
+-- | Deserialize a set of 'IndexImpl's. Requires a set of available index implementations.
+--
+--   /Note/: This will fail if a used index implementation is not provided.
+gets' :: [IndexImpl Occurrences] -> Get [(Context, IndexImpl Occurrences)]
+gets' ts = do
+  n <- get :: Get Int
+  go [] n
   where
   go xs 0 = return $! reverse xs
   go xs i = do
-    x <- liftM2 (,) get (get'' ts)
+    x <- liftM2 (,) get (get' ts)
     x `seq` go (x:xs) (i-1)
 
-get'' :: [IndexImpl Occurrences] -> Get (IndexImpl Occurrences)
-get'' ts = do
+-- | Deserialize an 'IndexImpl'. Requires a set of available index implementations.
+--
+--   /Note/: This will fail if a used index implementation is not provided.
+get' :: [IndexImpl Occurrences] -> Get (IndexImpl Occurrences)
+get' ts = do
   t <- get :: Get TypeRep
   case L.find (\(IndexImpl i) -> t == typeOf i) ts of
     Just (IndexImpl x) -> IndexImpl <$> get `asTypeOf` return x
@@ -73,11 +95,7 @@ get'' ts = do
 
 -- ------------------------------------------------------------
 
--- | IndexImpl is the Wrapper for external access
---   we set the key to Text here, but allow internal
---   Key in all haskell types. For conversion we have
---   could imagine a normalization proxy implemented
---   with the KeyIndex Proxy
+-- | Wrap an index using @ExistentialQuantification@ tto allow heterogeneous containers.
 mkIndex :: IndexImplCon i v => i v -> IndexImpl v
 mkIndex i = IndexImpl $! i
 

hunt-searchengine/src/Hunt/Index/InvertedIndex.hs
@@ -5,8 +5,22 @@
 {-# LANGUAGE OverlappingInstances       #-}
 {-# LANGUAGE TypeFamilies               #-}
 
+-- ----------------------------------------------------------------------------
+{- |
+  Top-level index implementations with 'Text' keys for
+
+    * text
+
+    * integers
+
+    * dates and
+
+    * geographic positions.
+-}
+-- ----------------------------------------------------------------------------
+
 module Hunt.Index.InvertedIndex
-( InvertedIndex(..)
+( InvertedIndex (..)
 , InvertedIndexDate
 , InvertedIndexInt
 , InvertedIndexPosition
@@ -38,220 +52,10 @@ import qualified Hunt.Index.Schema.Normalize.Position     as Pos
 import           Data.Bijection
 
 -- ------------------------------------------------------------
--- inverted index using int proxy for numeric data
+-- Inverted index using text key
 -- ------------------------------------------------------------
 
--- newtype required to enable different Text->Text Bijection instances
-newtype UnInt = UnInt { unInt :: Text }
-  deriving (Show, Eq, NFData)
-
-instance Bijection UnInt Text where
-  to   = Int.denormalizeFromText . unInt
-  from = UnInt . Int.normalizeToText
-
-instance Bijection Text UnInt where
-  to   = UnInt
-  from = unInt
-
-newtype InvertedIndexInt v
-  = InvIntIx { invIntIx :: KeyProxyIndex Text (KeyProxyIndex UnInt InvertedIndex) v }
-  deriving (Eq, Show, NFData, Typeable)
-
-mkInvIntIx :: KeyProxyIndex Text (KeyProxyIndex UnInt InvertedIndex) v -> InvertedIndexInt v
-mkInvIntIx x = InvIntIx $! x
-
--- ------------------------------------------------------------
-
-instance Binary (InvertedIndexInt v) where
-  put = put . invIntIx
-  get = get >>= return . InvIntIx
-
--- ------------------------------------------------------------
-
-instance Index InvertedIndexInt where
-  type IKey InvertedIndexInt v = Text
-  type IVal InvertedIndexInt v = Occurrences
-
-  insertList wos (InvIntIx i)
-    = mkInvIntIx $ insertList wos i
-
-  deleteDocs docIds (InvIntIx i)
-    = mkInvIntIx $ deleteDocs docIds i
-
-  empty
-    = mkInvIntIx $ empty
-
-  fromList l
-    = mkInvIntIx $ fromList l
-
-  toList (InvIntIx i)
-    = toList i
-
-  search t k (InvIntIx i)
-    = search t k i
-
-  lookupRange k1 k2 (InvIntIx i)
-    = lookupRange k1 k2 i
-
-  unionWith op (InvIntIx i1) (InvIntIx i2)
-    = mkInvIntIx $ unionWith op i1 i2
-
-  unionWithConv to' f (InvIntIx i1) (InvIntIx i2)
-    = mkInvIntIx $ unionWithConv to' f i1 i2
-
-  map f (InvIntIx i)
-    = mkInvIntIx $ Ix.map f i
-
-  mapMaybe f (InvIntIx i)
-    = mkInvIntIx $ Ix.mapMaybe f i
-
-  keys (InvIntIx i)
-    = keys i
-
-
--- ------------------------------------------------------------
--- inverted index using date proxy for date information
--- ------------------------------------------------------------
-newtype UnDate = UnDate { unDate :: Text }
-  deriving (Show, Eq, NFData)
-
-instance Bijection UnDate Text where
-  to   = Date.denormalize . unDate
-  from = UnDate . Date.normalize
-
-instance Bijection Text UnDate where
-  to   = UnDate
-  from = unDate
-
-newtype InvertedIndexDate v
-  = InvDateIx { invDateIx :: KeyProxyIndex Text (KeyProxyIndex UnDate InvertedIndex) v }
-  deriving (Eq, Show, NFData, Typeable)
-
-mkInvDateIx :: KeyProxyIndex Text (KeyProxyIndex UnDate InvertedIndex) v -> InvertedIndexDate v
-mkInvDateIx x = InvDateIx $! x
-
--- ------------------------------------------------------------
-
-instance Binary (InvertedIndexDate v) where
-  put = put . invDateIx
-  get = get >>= return . mkInvDateIx
-
--- ------------------------------------------------------------
-
-instance Index InvertedIndexDate where
-  type IKey InvertedIndexDate v = Word
-  type IVal InvertedIndexDate v = Occurrences
-
-  insertList wos (InvDateIx i)
-    = mkInvDateIx $ insertList wos i
-
-  deleteDocs docIds (InvDateIx i)
-    = mkInvDateIx $ deleteDocs docIds i
-
-  empty
-    = mkInvDateIx $ empty
-
-  fromList l
-    = mkInvDateIx $ fromList l
-
-  toList (InvDateIx i)
-    = toList i
-
-  search t k (InvDateIx i)
-    = search t k i
-
-  lookupRange k1 k2 (InvDateIx i)
-    = lookupRange k1 k2 i
-
-  unionWith op (InvDateIx i1) (InvDateIx i2)
-    = mkInvDateIx $ unionWith op i1 i2
-
-  unionWithConv to' f (InvDateIx i1) (InvDateIx i2)
-    = mkInvDateIx $ unionWithConv to' f i1 i2
-
-  map f (InvDateIx i)
-    = mkInvDateIx $ Ix.map f i
-
-  mapMaybe f (InvDateIx i)
-    = mkInvDateIx $ Ix.mapMaybe f i
-
-  keys (InvDateIx i)
-    = keys i
-
--- ------------------------------------------------------------
--- inverted index using position proxy for geo coordinates
--- ------------------------------------------------------------
-newtype UnPos = UnPos { unPos :: Text }
-  deriving (Show, Eq, NFData)
-
-instance Bijection UnPos Text where
-  to   = Pos.denormalize . unPos
-  from = UnPos . Pos.normalize
-
-instance Bijection Text UnPos where
-  to   = UnPos
-  from = unPos
-
-newtype InvertedIndexPosition v
-  = InvPosIx { invPosIx :: KeyProxyIndex Text (KeyProxyIndex UnPos InvertedIndex2Dim) v }
-  deriving (Eq, Show, NFData, Typeable)
-
-mkInvPosIx :: KeyProxyIndex Text (KeyProxyIndex UnPos InvertedIndex2Dim) v -> InvertedIndexPosition v
-mkInvPosIx x = InvPosIx $! x
-
--- ------------------------------------------------------------
-
-instance Binary (InvertedIndexPosition v) where
-  put = put . invPosIx
-  get = get >>= return . mkInvPosIx
-
--- ------------------------------------------------------------
-
-instance Index InvertedIndexPosition where
-  type IKey InvertedIndexPosition v = Word
-  type IVal InvertedIndexPosition v = Occurrences
-
-  insertList wos (InvPosIx i)
-    = mkInvPosIx $ insertList wos i
-
-  deleteDocs docIds (InvPosIx i)
-    = mkInvPosIx $ deleteDocs docIds i
-
-  empty
-    = mkInvPosIx $ empty
-
-  fromList l
-    = mkInvPosIx $ fromList l
-
-  toList (InvPosIx i)
-    = toList i
-
-  search t k (InvPosIx i)
-    = search t k i
-
-  lookupRange k1 k2 (InvPosIx i)
-    = lookupRange k1 k2 i
-
-  unionWith op (InvPosIx i1) (InvPosIx i2)
-    = mkInvPosIx $ unionWith op i1 i2
-
-  unionWithConv to' f (InvPosIx i1) (InvPosIx i2)
-    = mkInvPosIx $ unionWithConv to' f i1 i2
-
-  map f (InvPosIx i)
-    = mkInvPosIx $ Ix.map f i
-
-  mapMaybe f (InvPosIx i)
-    = mkInvPosIx $ Ix.mapMaybe f i
-
-  keys (InvPosIx i)
-    = keys i
-
-
--- ------------------------------------------------------------
--- default inverted index using text key
--- ------------------------------------------------------------
-
+-- | Text index using a 'StringMap'-implementation.
 newtype InvertedIndex _v
   = InvIx { invIx :: KeyProxyIndex Text ComprOccPrefixTree Occurrences }
   deriving (Eq, Show, NFData, Typeable)
@@ -312,10 +116,12 @@ instance Index InvertedIndex where
   keys (InvIx i)
     = keys i
 
+
 -- ------------------------------------------------------------
--- 2dim inverted index using text key
+-- Inverted index using 2-dimensional lookup
 -- ------------------------------------------------------------
 
+-- | Text index with 2-dimensional lookup using a 'StringMap'-implementation.
 newtype InvertedIndex2Dim _v
   = InvIx2D { invIx2D :: KeyProxyIndex Text PT2D.ComprOccPrefixTree Occurrences }
   deriving (Eq, Show, NFData, Typeable)
@@ -374,6 +180,231 @@ instance Index InvertedIndex2Dim where
     = mkInvIx2D $ Ix.mapMaybe f i
 
   keys (InvIx2D i)
+    = keys i
+
+
+-- ------------------------------------------------------------
+-- Inverted index using int proxy for numeric data
+-- ------------------------------------------------------------
+
+-- | Newtype to allow integer normalization 'Bijection' instance.
+newtype UnInt = UnInt { unInt :: Text }
+  deriving (Show, Eq, NFData)
+
+instance Bijection UnInt Text where
+  to   = Int.denormalizeFromText . unInt
+  from = UnInt . Int.normalizeToText
+
+instance Bijection Text UnInt where
+  to   = UnInt
+  from = unInt
+
+-- ------------------------------------------------------------
+
+-- | Integer index using a 'StringMap'-implementation.
+newtype InvertedIndexInt v
+  = InvIntIx { invIntIx :: KeyProxyIndex Text (KeyProxyIndex UnInt InvertedIndex) v }
+  deriving (Eq, Show, NFData, Typeable)
+
+mkInvIntIx :: KeyProxyIndex Text (KeyProxyIndex UnInt InvertedIndex) v -> InvertedIndexInt v
+mkInvIntIx x = InvIntIx $! x
+
+-- ------------------------------------------------------------
+
+instance Binary (InvertedIndexInt v) where
+  put = put . invIntIx
+  get = get >>= return . InvIntIx
+
+-- ------------------------------------------------------------
+
+instance Index InvertedIndexInt where
+  type IKey InvertedIndexInt v = Text
+  type IVal InvertedIndexInt v = Occurrences
+
+  insertList wos (InvIntIx i)
+    = mkInvIntIx $ insertList wos i
+
+  deleteDocs docIds (InvIntIx i)
+    = mkInvIntIx $ deleteDocs docIds i
+
+  empty
+    = mkInvIntIx $ empty
+
+  fromList l
+    = mkInvIntIx $ fromList l
+
+  toList (InvIntIx i)
+    = toList i
+
+  search t k (InvIntIx i)
+    = search t k i
+
+  lookupRange k1 k2 (InvIntIx i)
+    = lookupRange k1 k2 i
+
+  unionWith op (InvIntIx i1) (InvIntIx i2)
+    = mkInvIntIx $ unionWith op i1 i2
+
+  unionWithConv to' f (InvIntIx i1) (InvIntIx i2)
+    = mkInvIntIx $ unionWithConv to' f i1 i2
+
+  map f (InvIntIx i)
+    = mkInvIntIx $ Ix.map f i
+
+  mapMaybe f (InvIntIx i)
+    = mkInvIntIx $ Ix.mapMaybe f i
+
+  keys (InvIntIx i)
+    = keys i
+
+
+-- ------------------------------------------------------------
+-- inverted index using date proxy for dates
+-- ------------------------------------------------------------
+
+-- | Newtype to allow date normalization 'Bijection' instance.
+newtype UnDate = UnDate { unDate :: Text }
+  deriving (Show, Eq, NFData)
+
+instance Bijection UnDate Text where
+  to   = Date.denormalize . unDate
+  from = UnDate . Date.normalize
+
+instance Bijection Text UnDate where
+  to   = UnDate
+  from = unDate
+
+-- ------------------------------------------------------------
+
+-- | Date index using a 'StringMap'-implementation.
+newtype InvertedIndexDate v
+  = InvDateIx { invDateIx :: KeyProxyIndex Text (KeyProxyIndex UnDate InvertedIndex) v }
+  deriving (Eq, Show, NFData, Typeable)
+
+mkInvDateIx :: KeyProxyIndex Text (KeyProxyIndex UnDate InvertedIndex) v -> InvertedIndexDate v
+mkInvDateIx x = InvDateIx $! x
+
+-- ------------------------------------------------------------
+
+instance Binary (InvertedIndexDate v) where
+  put = put . invDateIx
+  get = get >>= return . mkInvDateIx
+
+-- ------------------------------------------------------------
+
+instance Index InvertedIndexDate where
+  type IKey InvertedIndexDate v = Word
+  type IVal InvertedIndexDate v = Occurrences
+
+  insertList wos (InvDateIx i)
+    = mkInvDateIx $ insertList wos i
+
+  deleteDocs docIds (InvDateIx i)
+    = mkInvDateIx $ deleteDocs docIds i
+
+  empty
+    = mkInvDateIx $ empty
+
+  fromList l
+    = mkInvDateIx $ fromList l
+
+  toList (InvDateIx i)
+    = toList i
+
+  search t k (InvDateIx i)
+    = search t k i
+
+  lookupRange k1 k2 (InvDateIx i)
+    = lookupRange k1 k2 i
+
+  unionWith op (InvDateIx i1) (InvDateIx i2)
+    = mkInvDateIx $ unionWith op i1 i2
+
+  unionWithConv to' f (InvDateIx i1) (InvDateIx i2)
+    = mkInvDateIx $ unionWithConv to' f i1 i2
+
+  map f (InvDateIx i)
+    = mkInvDateIx $ Ix.map f i
+
+  mapMaybe f (InvDateIx i)
+    = mkInvDateIx $ Ix.mapMaybe f i
+
+  keys (InvDateIx i)
+    = keys i
+
+
+-- ------------------------------------------------------------
+-- Inverted index using position proxy for geo coordinates
+-- ------------------------------------------------------------
+
+-- | Newtype to allow position normalization 'Bijection' instance.
+newtype UnPos = UnPos { unPos :: Text }
+  deriving (Show, Eq, NFData)
+
+instance Bijection UnPos Text where
+  to   = Pos.denormalize . unPos
+  from = UnPos . Pos.normalize
+
+instance Bijection Text UnPos where
+  to   = UnPos
+  from = unPos
+
+-- ------------------------------------------------------------
+
+-- | Geo-position index using a 'StringMap'-implementation.
+newtype InvertedIndexPosition v
+  = InvPosIx { invPosIx :: KeyProxyIndex Text (KeyProxyIndex UnPos InvertedIndex2Dim) v }
+  deriving (Eq, Show, NFData, Typeable)
+
+mkInvPosIx :: KeyProxyIndex Text (KeyProxyIndex UnPos InvertedIndex2Dim) v -> InvertedIndexPosition v
+mkInvPosIx x = InvPosIx $! x
+
+-- ------------------------------------------------------------
+
+instance Binary (InvertedIndexPosition v) where
+  put = put . invPosIx
+  get = get >>= return . mkInvPosIx
+
+-- ------------------------------------------------------------
+
+instance Index InvertedIndexPosition where
+  type IKey InvertedIndexPosition v = Word
+  type IVal InvertedIndexPosition v = Occurrences
+
+  insertList wos (InvPosIx i)
+    = mkInvPosIx $ insertList wos i
+
+  deleteDocs docIds (InvPosIx i)
+    = mkInvPosIx $ deleteDocs docIds i
+
+  empty
+    = mkInvPosIx $ empty
+
+  fromList l
+    = mkInvPosIx $ fromList l
+
+  toList (InvPosIx i)
+    = toList i
+
+  search t k (InvPosIx i)
+    = search t k i
+
+  lookupRange k1 k2 (InvPosIx i)
+    = lookupRange k1 k2 i
+
+  unionWith op (InvPosIx i1) (InvPosIx i2)
+    = mkInvPosIx $ unionWith op i1 i2
+
+  unionWithConv to' f (InvPosIx i1) (InvPosIx i2)
+    = mkInvPosIx $ unionWithConv to' f i1 i2
+
+  map f (InvPosIx i)
+    = mkInvPosIx $ Ix.map f i
+
+  mapMaybe f (InvPosIx i)
+    = mkInvPosIx $ Ix.mapMaybe f i
+
+  keys (InvPosIx i)
     = keys i
 
 -- ------------------------------------------------------------

commit 6cfa9f4894035f4a725dae4c3b2984c440d0e30f

Fix serialization bug introduced with refactored code
Introduced with 717abc05917814f143c0a7789ba1534c70716235

hunt-searchengine/src/Hunt/Index/Proxy/KeyIndex.hs
@@ -56,7 +56,7 @@ mkKeyProxyIx v = KeyProxyIx $! v
 -- ------------------------------------------------------------
 
 instance Binary (impl v) => Binary (KeyProxyIndex toType impl v) where
-  put = put . KeyProxyIx
+  put = put . keyProxyIx
   get = get >>= return . mkKeyProxyIx
 
 -- ------------------------------------------------------------