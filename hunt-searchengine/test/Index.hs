32175c141e0be8bc5f21b043f271aa2c55bf12d7
Chris Reumann <chrisreumann@googlemail.com>
2014-04-10 15:13:27 +0200

    add index tests

--- hunt-searchengine/test/Index.hs
+++ hunt-searchengine/test/Index.hs
@@ -12,6 +12,7 @@ import           Test.Framework.Providers.HUnit
 import           Test.HUnit
 
 import           Hunt.Common.BasicTypes
+import           Hunt.Common.DocId                           (DocId)
 import qualified Hunt.Common.DocIdMap                        as DM
 import           Hunt.Common.DocIdMap                        (DocIdMap)
 import           Hunt.Common.Positions                       (Positions)
@@ -30,48 +31,58 @@ import qualified Hunt.Index.PrefixTreeIndex                  as PIx
 
 main :: IO ()
 main = defaultMainWithOpts
-  [ testCase "DmPrefixTree:            insert"        insertTestPIx
+  [
+  -- TODO
+  -- test: insertList, deleteDocs, toList, fromList, map
+  -- test: intindex, dateindex, geoindex 
+
+  -- PrefixTree implementation
+    testCase "DmPrefixTree:            insert"        insertTestPIx
+  , testCase "DmPrefixTree:            delete"        deleteTestPIx
+  , testCase "DmPrefixTree:            merge"         mergeTestPIx
+
+  -- InvertedIndex implementation
   , testCase "InvertedIndex:           insert"        insertTestInvIx
   , testCase "InvertedIndex:           deleteDocs"    deleteDocsTestInvIx
   , testCase "InvertedIndex:           merge"         mergeTestInvIx
-  , testCase "ContextMap Inverted:     insert"        insertTestContextIx
-  , testCase "ContextMap Inverted:     insertContext" insertTestContext
+
+  -- ContextIndex implementation
+  , testCase "ContextIndex:            insert"        insertTestContextIx
+  , testCase "ContextIndex:            insertContext" insertTestContext
+  -- helper functions
   , testCase "TextIndex:               addWords"      addWordsTest
+  , testCase "Occurrences:             merge"         occMergeTest
   ] mempty
 
--- check if element is inserted in insert operation
+-- ----------------------------------------------------------------------------
+-- Check DmPrefixTree implementation
 
--- | general check function
-insertTest :: (Monad m, Ix.Index i, Eq (Ix.IVal i v), (Ix.ICon i v)) =>
-              i v -> Ix.IKey i v -> Ix.IVal i v -> m Bool
-insertTest emptyIndex k v = do
-  ix       <- Ix.insertM k v emptyIndex
-  [(_,nv)] <- Ix.searchM PrefixNoCase k ix
-  return $ v == nv
-
-
--- v1 and v2 need the have the same id
-mergeTest :: (Monad m, Ix.ICon i v, Ix.Index i
-             , Ix.IVal i v ~ DocIdMap Positions) =>
-             i v -> Ix.IKey i v -> Occurrences -> Occurrences -> m Bool
-mergeTest emptyIndex k v1 v2 = do
-  mergeIx      <- Ix.insertM k v1 emptyIndex >>= \ix -> Ix.insertM k v2 ix
-  res@[(_,nv)] <- Ix.searchM PrefixNoCase k mergeIx
-  return $ length res == 1 && merge v1 v2 == nv
-
-
--- | check DmPrefixTree
+-- | check insert on DmPrefixTree
 insertTestPIx :: Assertion
 insertTestPIx = do
   result <- insertTest (Ix.empty::(PIx.DmPrefixTree Positions)) "test" (singleton 1 1)
   True @?= result
 
+deleteTestPIx :: Assertion
+deleteTestPIx = do
+  result <- deleteTest (Ix.empty::(PIx.DmPrefixTree Positions)) "test" (singleton docId 1) docId
+  True @?= result
+  where
+  docId = 1
 
--- | check InvertedIndex
+mergeTestPIx :: Assertion
+mergeTestPIx = do
+  result <- mergeTest (Ix.empty::(PIx.DmPrefixTree Positions)) "test" (singleton 1 1) (singleton 1 2)
+  True @?= result
+
+-- ----------------------------------------------------------------------------
+-- Check InvertedIndex implementation
+
+-- | check insert InvertedIndex
 insertTestInvIx :: Assertion
 insertTestInvIx = do
   result <- insertTest
-            (Ix.empty::(InvIx.InvertedIndex Occurrences)) -- the Occurrences type is a dummy in this case
+            (Ix.empty::(InvIx.InvertedIndex Occurrences))
             "test" (singleton 1 1)
   True @?= result
 
@@ -91,15 +102,26 @@ deleteDocsTestInvIx = do
   result <- Ix.searchM PrefixNoCase "test" ix''
   result @?= []
 
--- | test merging of occurrences in InvertedIndex
+-- | test merging of Occurrences in InvertedIndex
 mergeTestInvIx :: Assertion
 mergeTestInvIx = do
   result <- mergeTest
-            (Ix.empty::(InvIx.InvertedIndex Occurrences)) -- the Occurrences type is a dummy in this case
+            (Ix.empty::(InvIx.InvertedIndex Occurrences))
             "test" (singleton 1 1) (singleton 1 2)
   True @?= result
 
--- | check ContextMap
+-- ----------------------------------------------------------------------------
+-- Check ContextIndex
+
+-- | check insert context on ContextIndex
+insertTestContext :: Assertion
+insertTestContext = "test" @?= insertedContext
+  where
+  [insertedContext] = ConIx.contexts' ix
+  ix :: ConIx.ContextMap Occurrences
+  ix = ConIx.insertContext "test" (mkIndex (Ix.empty :: InvIx.InvertedIndex Occurrences)) ConIx.empty
+
+-- | check insert on ContextIndex
 insertTestContextIx :: Assertion
 insertTestContextIx
   = do
@@ -114,12 +136,6 @@ insertTestContextIx
   impl       :: InvIx.InvertedIndex Occurrences
   impl       = Ix.empty
 
-insertTestContext :: Assertion
-insertTestContext = "test" @?= insertedContext
-  where
-  [insertedContext] = ConIx.contexts' ix
-  ix :: ConIx.ContextMap Occurrences
-  ix = ConIx.insertContext "test" (mkIndex (Ix.empty :: InvIx.InvertedIndex Occurrences)) ConIx.empty
 
 -- ----------------------------------------------------------------------------
 -- check helper functions
@@ -136,8 +152,15 @@ addWordsTest = do
   impl       :: InvIx.InvertedIndex Occurrences
   impl       = Ix.empty
 
+occMergeTest :: Assertion
+occMergeTest = True @?= (merge occ1 occ2 == occ3)
+  where
+  occ1 = singleton 1 1
+  occ2 = singleton 1 2
+  occ3 = insert 1 2 $ singleton 1 1
+
 -- ----------------------------------------------------------------------------
--- helper
+-- test helper
 -- ----------------------------------------------------------------------------
 
 wordList :: WordList
@@ -148,3 +171,30 @@ wrds w = M.fromList $ [(w, wordList)]
 
 doc :: Document
 doc = Document "id::1" (DD.fromList [("name", "Chris"), ("alter", "30")]) 1.0
+
+
+-- | Generic insert test function
+insertTest :: (Monad m, Ix.Index i, Eq (Ix.IVal i v), (Ix.ICon i v)) =>
+              i v -> Ix.IKey i v -> Ix.IVal i v -> m Bool
+insertTest emptyIndex k v = do
+  ix       <- Ix.insertM k v emptyIndex
+  [(_,nv)] <- Ix.searchM PrefixNoCase k ix
+  return $ v == nv
+
+-- | Generic delete test function
+deleteTest :: (Monad m, Ix.Index i, Eq (Ix.IVal i v), (Ix.ICon i v)) =>
+              i v -> Ix.IKey i v -> Ix.IVal i v -> DocId -> m Bool
+deleteTest emptyIndex k v did = do
+  ix       <- Ix.insertM k v emptyIndex
+  [(_,nv)] <- Ix.searchM PrefixNoCase k ix
+  ix'      <- Ix.deleteM did ix
+  return $ v == nv && Prelude.null (Ix.toList ix')
+
+-- | Generic Occurrence merge function
+mergeTest :: (Monad m, Ix.ICon i v, Ix.Index i, (Eq (Ix.IKey i v))
+             , Ix.IVal i v ~ DocIdMap Positions) =>
+             i v -> Ix.IKey i v -> Occurrences -> Occurrences -> m Bool
+mergeTest emptyIndex k v1 v2 = do
+  mergeIx      <- Ix.insertM k v2 $ Ix.insert k v1 emptyIndex
+  [(nk,nv)] <- Ix.searchM PrefixNoCase k mergeIx
+  return $ nk == k && (merge v1 v2) == nv
