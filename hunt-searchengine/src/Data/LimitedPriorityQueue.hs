bbb4e1fd6de4b015a5fc8f3795039c2b40b3a70e
Uwe Schmidt <si@fh-wedel.de>
2014-08-14 17:17:15 +0200

    LimitedPriorityQueue refactored to work with arbitrary values, not only pairs of score and value
    Document ranking and word ranking is done by wrapping documents and word-score pairs into
    a new data type and define an appropriate Ord instance for these types

--- hunt-searchengine/src/Data/LimitedPriorityQueue.hs
+++ hunt-searchengine/src/Data/LimitedPriorityQueue.hs
@@ -10,7 +10,7 @@
 
 -}
 
-module Hunt.Common.LimitedPriorityQueue
+module Data.LimitedPriorityQueue
     ( Queue
     , mkQueue
     , insert
@@ -21,8 +21,7 @@ module Hunt.Common.LimitedPriorityQueue
     )
 where
 
-import           Hunt.Common.BasicTypes (Score)
-import           Prelude                hiding (drop, take)
+import           Prelude hiding (drop, take)
 
 -- ----------------------------------------------------------------------------
 
@@ -35,11 +34,11 @@ data Queue v
 
 data Heap v
     = E
-    | T !Score v (Heap v) (Heap v)
+    | T v (Heap v) (Heap v)
       deriving (Show)
 
 -- | Create an empty priority queue with a limited capacity.
--- If capacity is < 0, no limit is defined.
+-- If capacity is < 0, no limit is set
 
 mkQueue :: Int -> Queue v
 mkQueue c
@@ -49,30 +48,30 @@ mkQueue c
         = Q maxBound 0 E
 
 -- | Insert an element if there is space in the queue
--- or if element is larger than smallest element.
+-- or if the element is larger than the smallest element.
 
-insert :: v -> Score -> Queue v -> Queue v
-insert di sc q@(Q c s h)
+insert :: Ord v => v -> Queue v -> Queue v
+insert x q@(Q c s h)
     | s < c                     -- capacity not reached: insert
-        = Q c (s + 1) (merge h (T sc di E E))
-    | sc' >= sc                 -- queue full, score < min score: don't insert
+        = Q c (s + 1) (merge h (T x E E))
+    | x' >= x                   -- queue full, score < min score: don't insert
         = q
     | otherwise                 -- queue full, score >= min: throw min away and insert
-        = Q c s (merge (merge l r) (T sc di E E))
+        = Q c s (merge (merge l r) (T x E E))
     where
-      (T sc' _di' l r) = h
+      (T x' l r) = h
 
 -- | Reduce size and capacity of queue
 -- by throwing away small elements.
 
-reduce :: Int -> Queue v -> Queue v
+reduce :: Ord v => Int -> Queue v -> Queue v
 reduce i (Q _ s h)
     | i >= s
         = Q i s h
     | otherwise -- i < s
         = Q i i (drop (s - i) h)
 
-toList :: Int -> Int -> Queue v -> [(v, Score)]
+toList :: Ord v => Int -> Int -> Queue v -> [v]
 toList start len (Q _ s h)
     | len < 0                   -- no limit on the length, take all except the first elems
         = take (s - start) h
@@ -83,9 +82,9 @@ toList start len (Q _ s h)
       where
         len' = start + len
 
-fromList :: Int -> [(v, Score)] -> Queue v
+fromList :: Ord v => Int -> [v] -> Queue v
 fromList c
-    = foldl (\ q (di, sc) -> insert di sc q) (mkQueue c)
+    = foldl (\ q x -> insert x q) (mkQueue c)
 
 -- | Take a list of scored values, sort it and return a page of the result.
 --
@@ -93,7 +92,7 @@ fromList c
 --
 -- If the length is set to @-1@ no limit on the page length is set.
 
-pageList :: Int -> Int -> [(v, Score)] -> [(v, Score)]
+pageList :: Ord v => Int -> Int -> [v] -> [v]
 pageList start len
     = toList start len . fromList len'
     where
@@ -104,35 +103,35 @@ pageList start len
 -- ----------------------------------------------------------------------------
 -- skew heap operations
 
-take :: Int -> Heap v -> [(v, Score)]
+take :: Ord v => Int -> Heap v -> [v]
 take i0 h0
     = take' i0 h0 []
     where
-      take' i (T sc di l r) acc
+      take' i (T x l r) acc
           | i <= 0
               = acc
           | otherwise
-              = take' (i - 1) (merge l r) ((di, sc) : acc)
+              = take' (i - 1) (merge l r) (x : acc)
       take' _ E acc
             = acc
 
-drop :: Int -> Heap v -> Heap v
+drop :: Ord v => Int -> Heap v -> Heap v
 drop i h
     | i <= 0
         = h
     | otherwise
         = drop (i - 1) (merge l r)
     where
-      (T _sc _di l r) = h
+      (T _x l r) = h
 
-merge :: Heap v -> Heap v -> Heap v
+merge :: Ord v => Heap v -> Heap v -> Heap v
 merge E q2 = q2
 merge q1 E = q1
-merge q1@(T sc1 di1 l1 r1) q2@(T sc2 di2 l2 r2)
-    | sc1 <= sc2
-        = T sc1 di1 r1 (merge l1 q2)
+merge q1@(T x1 l1 r1) q2@(T x2 l2 r2)
+    | x1 <= x2
+        = T x1 r1 (merge l1 q2)
     | otherwise
-        = T sc2 di2 r2 (merge l2 q1)
+        = T x2 r2 (merge l2 q1)
 
 -- ----------------------------------------------------------------------------
 
