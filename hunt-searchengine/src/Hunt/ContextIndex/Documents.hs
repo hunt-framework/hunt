7782c9451974393d46b65da185918972b4ceec29
Alex Biehl <alexbiehl@gmail.com>
2015-05-10 01:23:42 +0200

    Cleanup

--- hunt-searchengine/src/Hunt/ContextIndex/Documents.hs
+++ hunt-searchengine/src/Hunt/ContextIndex/Documents.hs
@@ -20,28 +20,31 @@ reduceMaybes' = listToMaybe . catMaybes
 
 lookupDocumentByURI :: (DocTable dt, Par.MonadParallel m) => URI -> ContextIndex dt -> m (Maybe DocId)
 lookupDocumentByURI uri ixx
-  = do dIds <- mapIxsP (\seg -> do di  <- DocTable.lookupByURI uri (segDocs seg)
-                                   case di of
-                                    Nothing -> return Nothing
-                                    Just dId | DocIdSet.member dId (segDeletedDocs seg)
-                                               -> return Nothing
-                                             | otherwise
-                                               -> return (Just dId)
-                   ) ixx
+  = do dIds <- mapIxsP docs ixx
        return (reduceMaybes' dIds)
+    where
+      docs seg
+        = do di  <- DocTable.lookupByURI uri (segDocs seg)
+             case di of
+              Just dId | not (docsMember dId seg) -> return (Just dId)
+              _                                   -> return Nothing
 
 lookupDocument :: (DocTable dt, Par.MonadParallel m) => ContextIndex dt -> DocId -> m (Maybe Document)
 lookupDocument ixx dId
-  = do docs <- mapIxsP (\seg -> if DocIdSet.member dId (segDeletedDocs seg)
-                                 then return Nothing
-                                 else DocTable.lookup dId (segDocs seg)
-                      ) ixx
-
+  = do docs <- mapIxsP doc ixx
        return (fmap Document.unwrap (reduceMaybes' docs))
+  where
+    doc seg = if docsMember dId seg
+              then return Nothing
+              else DocTable.lookup dId (segDocs seg)
 
 selectDocuments :: (Applicative m, Par.MonadParallel m, DocTable dt) => ContextIndex dt -> DocIdSet -> m dt
 selectDocuments ixx dIds
-  = do dtx <- mapIxsP (\seg -> do dt <- DocTable.restrict dIds (segDocs seg)
-                                  DocTable.difference (segDeletedDocs seg) dt
-                     ) ixx
+  = do dtx <- mapIxsP docs ixx
        foldM DocTable.union DocTable.empty dtx
+  where
+    docs seg
+      = do dt <- DocTable.restrict dIds (segDocs seg)
+           DocTable.difference (segDeletedDocs seg) dt
+
+docsMember dId seg = DocIdSet.member dId (segDeletedDocs seg)
