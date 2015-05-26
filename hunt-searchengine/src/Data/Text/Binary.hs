8cbf931b0d80a52946e63223b310e3d0179c400f
alexbiehl <alex.biehl@gmail.com>
2015-05-26 14:24:31 +0200

    Text binary workaround

--- hunt-searchengine/src/Data/Text/Binary.hs
+++ hunt-searchengine/src/Data/Text/Binary.hs
@@ -1,5 +1,5 @@
+{-# LANGUAGE CPP #-}
 {-# OPTIONS -fno-warn-orphans #-}
-
 -- ----------------------------------------------------------------------------
 {- |
 Default 'Binary' instance for 'Text' using UTF-8.
@@ -8,6 +8,8 @@ Default 'Binary' instance for 'Text' using UTF-8.
 
 module Data.Text.Binary
 where
+
+#if !(MIN_VERSION_text(1,2,1))
 
 import           Control.Monad      (liftM)
 import           Data.Binary        (Binary (..))
@@ -19,5 +21,6 @@ import           Data.Text.Encoding
 instance Binary Text where
   put = put . encodeUtf8
   get = liftM decodeUtf8 get
+#endif
 
 -- ------------------------------------------------------------
