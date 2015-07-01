b5d45decf87b87058156d5f66dda9662f037af4d
Alex Biehl <abiehl@novomind.com>
2015-07-01 16:23:44 +0200

    Fix lazyness issue

--- hunt-fst/benchmarks/Trie.hs
+++ hunt-fst/benchmarks/Trie.hs
@@ -8,6 +8,7 @@ import           Control.Arrow
 import           Control.DeepSeq
 import           Criterion
 import           Criterion.Main
+import qualified Data.List as List
 import qualified Data.StringMap as SM
 import           Data.Text (Text)
 import qualified Data.Text as Text
@@ -18,12 +19,13 @@ loadWords
   = do big   <- Text.lines <$> Text.readFile "benchmarks/big.txt"
        small <- Text.lines <$> Text.readFile "benchmarks/small.txt"
        let mkPairs = fmap (\x -> (,) x x)
-       return $!! (mkPairs small, mkPairs big)
+           cleanup = fmap Text.strip . filter (not . Text.null)
 
+       return $!! (mkPairs $ small, mkPairs $ big)
 
 loadWordsSM :: IO ([(String, Text)], ([(String, Text)]))
 loadWordsSM
-  = do big <- Text.lines <$> Text.readFile "benchmarks/big.txt"
+  = do big   <- Text.lines <$> Text.readFile "benchmarks/big.txt"
        small <- Text.lines <$> Text.readFile "benchmarks/small.txt"
        let mkPairs = fmap (\x -> (,) x x)
            toKey   = fmap (first Text.unpack)
--- hunt-fst/src/Hunt/FST/Compile.hs
+++ hunt-fst/src/Hunt/FST/Compile.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE CPP #-}
-{-# OPTIONS_GHC -fsimpl-tick-factor=500 #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fsimpl-tick-factor=500 -O0 #-}
 module Hunt.FST.Compile where
 
 import           Prelude hiding (head, tail)
@@ -17,13 +17,22 @@ import           Control.Monad.ST
 import           Data.Text (Text)
 import qualified Data.Text.Array as Array
 import qualified Data.Text.Internal as Text
+import Data.Vector.Fusion.Stream (liftStream)
 import           Data.Vector.Fusion.Stream.Monadic as Stream
 import           Data.Vector.Fusion.Stream.Size as Stream
 import qualified Data.Vector.Internal.Check as Ck
+import Data.Vector.Generic (stream, unstream)
+import Data.Vector.Generic.Mutable (mstream, munstream)
+import Unsafe.Coerce
+import qualified Data.Vector.Mutable as MVector
+import qualified Data.Vector as Vector
 
 import           Data.Word (Word8)
 import           Data.Primitive.MutVar
 import qualified Data.Vector.Unboxed as UVector
+
+import Debug.Trace
+
 
 #define ERROR (Ck.error __FILE__ __LINE__)
 #define EMPTY_STREAM (\sx123 -> ERROR sx123 "emptyStream")
@@ -53,29 +62,28 @@ compileSuffix :: PrimMonad m
 compileSuffix register (Stream next1 x1 _)
   = loop0 SPEC (CS0 x1)
   where
-    loop0 !sPEC (CS0 s)
+    loop0 !sPEC (CS0 !s)
       = do r <- next1 s
            case r of
-            Yield (UncompiledState l a o) s' -> do
-              arc <- loop1 SPEC (CS1 (UncompiledState l a o) s')
-              replaceOrRegister (UncompiledState l (arc `Arcs.cons` a) o) register
-            Skip s' -> loop0 SPEC (CS0 s')
+            Yield (UncompiledState !l !a !o) !s' -> do
+              !arc <- loop1 SPEC (CS1 (UncompiledState l a o) s')
+              arc `seq` replaceOrRegister (UncompiledState l (arc `Arcs.cons` a) o) register
+            Skip !s' -> loop0 SPEC (CS0 s')
             Done    -> EMPTY_STREAM "compileStream"
-    loop1 !sPEC (CS1 uc s)
+    loop1 !sPEC (CS1 !uc !s)
       = do r <- next1 s
            case r of
-            Yield uc'@(UncompiledState l a o) s' -> do
-              arc <- loop1 SPEC (CS1 uc' s')
-              replaceOrRegister (UncompiledState l (arc `Arcs.cons` a) o) register
-            Skip s' -> loop1 SPEC (CS1 uc s')
+            Yield uc'@(UncompiledState !l !a !o) !s' -> do
+              !arc <- loop1 SPEC (CS1 uc' s')
+              arc `seq` replaceOrRegister (UncompiledState l (arc `Arcs.cons` a) o) register
+            Skip !s' -> loop1 SPEC (CS1 uc s')
             Done    -> replaceOrRegister uc register
 {-# INLINE compileSuffix #-}
 
 data C a b c
   = C0 !b !c
   | C1 !(UncompiledState a) !b !c
-  | C2 !(UncompiledState a) !c
-  | C3 !c
+  | C2 !c
 
 compile :: PrimMonad m
         => Register (PrimState m) a
@@ -86,13 +94,13 @@ compile register _old@(Stream next1 x1 n1) _new@(Stream next2 x2 n2)
   = Stream next (C0 x1 x2)  (n1 + n2)
   where
     {-# INLINE next #-}
-    next (C0 s1 s2)
+    next (C0 !s1 !s2)
       = do r1 <- next1 s1
            case r1 of
             Yield x s1' -> return $ Skip (C1 x s1' s2)
             Skip s1'    -> return $ Skip (C0 s1' s2)
             Done        -> EMPTY_STREAM "compile: empty stream"
-    next (C1 prev s1 s2)
+    next (C1 !prev !s1 !s2)
       = do r1 <- next1 s1
            case r1 of
             Yield x s1' -> do
@@ -101,20 +109,18 @@ compile register _old@(Stream next1 x1 n1) _new@(Stream next2 x2 n2)
                Yield y s2'
                  | ucLabel x == ucLabel y -> return $ Yield prev (C1 x s1' s2')
                  | otherwise              -> do
-                     arc <- compileSuffix register (Stream next1 s1 n1)
-                     return $ Yield (prev {ucArcs = arc `Arcs.cons` ucArcs prev }) (C3 s2')
+                     !arc <- compileSuffix register (Stream next1 s1 n1)
+                     return $ Yield (prev {ucArcs = arc `Arcs.cons` ucArcs prev }) (C2 s2)
                Done        -> EMPTY_STREAM "compile: not in lexicographic order"
                Skip s2'    -> return $ Skip (C1 prev s1 s2')
-            Done        -> return $ Skip (C2 prev s2)
+            Done        -> return $ Yield prev (C2 s2)
             Skip s1'    -> return $ Skip (C1 prev s1' s2)
-    next (C2 prev s2)
-      = return $ Yield prev (C3 s2)
-    next (C3 s2)
+    next (C2 !s2)
       = do r2 <- next2 s2
            case r2 of
-            Yield x s2' -> return $ Yield x (C3 s2')
+            Yield x s2' -> return $ Yield x (C2 s2')
             Done        -> return   Done
-            Skip s2'    -> return $ Skip (C3 s2')
+            Skip s2'    -> return $ Skip (C2 s2')
 {-# INLINE compile #-}
 
 compileList :: PrimMonad m
@@ -122,15 +128,19 @@ compileList :: PrimMonad m
             -> [(Text, a)]
             -> m StateRef
 compileList register
-  = loop rootState
+  = do loop rootState
   where
     loop path []
-      = do rootArc <- compileSuffix register path
+      = do rootArc <- compileSuffix register (liftStream (stream path))
            return (Arcs.arcTarget rootArc)
     loop path ((w, a):ex)
-      = loop (compile register path (mkUncompiledState w a)) ex
+      = do buf  <- munstream path'
+           buf' <- Vector.unsafeFreeze buf
+           loop buf' ex
+      where
+        path' = compile register (liftStream (stream path)) (mkUncompiledState w a)
     rootState
-      = singleton (UncompiledState maxBound Arcs.empty Nothing)
+      = Vector.singleton (UncompiledState maxBound Arcs.empty Nothing)
 {-# INLINE compileList #-}
 
 compileList' :: [(Text, a)] -> UVector.Vector Word8
