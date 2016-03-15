8bb13dfb531c616e0482a67f9725c91addc22ffb
Alex Biehl <abiehl@novomind.com>
2016-03-15 15:47:41 +0100

    Introduce IndexAction types

--- hunt-searchengine/src/Hunt/ContextIndex/Types.hs
+++ hunt-searchengine/src/Hunt/ContextIndex/Types.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ExistentialQuantification #-}
 module Hunt.ContextIndex.Types where
 
 import Hunt.Common.SegmentMap (SegmentId, SegmentMap)
@@ -7,11 +8,9 @@ import Hunt.Index.Schema (Schema)
 import Hunt.Segment (Segment)
 
 import Control.DeepSeq
-import Data.Aeson
 import Data.Binary
 import Data.Monoid
 import Data.Set (Set)
-import Data.Typeable
 
 -- | The actual index type.
 data ContextIndex dt
@@ -22,8 +21,31 @@ data ContextIndex dt
                  , ciDirtiness     :: !Dirtiness
                  }
 
+instance NFData dt => NFData (ContextIndex dt) where
+  rnf ixx = rnf (ciActiveSegment ixx)
+    `seq` rnf (ciSegments ixx)
+    `seq` rnf (ciSchema ixx)
+    `seq` rnf (ciNextSegmentId ixx)
+
+-- | TODO: remove dummy instance
+instance Binary (ContextIndex dt) where
+   get = undefined
+   put = undefined
+
+-- |The ContxtIndex knows best when Segments need merging
+-- or flushing. An index action can be performed asynchrously
+-- as long as the ContextIndex is eventually applied to the result
+-- of an index action. Invariantly, the resulting function needs
+-- to be idempotent. This hopefully obsoletes the dirtiness check.
+data IndexAction dt =
+  IndexAction { runIxAction :: IO (ContextIndex dt -> ContextIndex dt) }
+
+instance Monoid (IndexAction dt) where
+  mempty = IndexAction (return id)
+  mappend x y = IndexAction $ (.) <$> runIxAction x <*> runIxAction y
+
 -- |Inserting a new segment into a ContextIndex changes its dirtiness.
---  This is used to signal Merger and Flusher to do some work.
+-- This is used to signal Merger and Flusher to do some work.
 data Dirtiness = IsDirty !(Set SegmentId)
                | NotDirty
                deriving (Eq)
@@ -34,14 +56,3 @@ instance Monoid Dirtiness where
   mappend (IsDirty xs) NotDirty     = IsDirty xs
   mappend NotDirty     (IsDirty xs) = IsDirty xs
   mappend _            _            = NotDirty
-
-instance NFData dt => NFData (ContextIndex dt) where
-  rnf ixx = rnf (ciActiveSegment ixx)
-    `seq` rnf (ciSegments ixx)
-    `seq` rnf (ciSchema ixx)
-    `seq` rnf (ciNextSegmentId ixx)
-
--- | TODO: remove dummy instance
-instance Binary (ContextIndex dt) where
-  get = undefined
-  put = undefined
