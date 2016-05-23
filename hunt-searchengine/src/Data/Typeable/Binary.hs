52072c13ccea46e7062c74b1b3d3f7f70231a3da
Alex Biehl <alexbiehl@gmail.com>
2016-05-23 21:30:19 +0200

    CPP macros for GHC8

--- hunt-searchengine/src/Data/Typeable/Binary.hs
+++ hunt-searchengine/src/Data/Typeable/Binary.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# OPTIONS -fno-warn-orphans #-}
 
 -- ----------------------------------------------------------------------------
@@ -21,5 +22,27 @@ instance Binary TypeRep where
 instance Binary TyCon where
   put (TyCon hash package modul name) = put hash >> put package >> put modul >> put name
   get = TyCon <$> get <*> get <*> get <*> get
+
+#elseif __GLASGOW_HASKELL__ < 800
+
+instance Binary TypeRep where
+  put (TypeRep fp tyCon kindRep tr) = put fp >> put tyCon >> put kindRep >> put tr
+  get = TypeRep <$> get <*> get <*> get <*> get
+
+instance Binary TyCon where
+  put (TyCon hash package modul name) = put hash >> put package >> put modul >> put name
+  get = TyCon <$> get <*> get <*> get <*> get
+
+#else
+
+instance Binary TypeRep where
+  put (TypeRep fp tyCon kindRep tr) = put fp >> put tyCon >> put kindRep >> put tr
+  get = TypeRep <$> get <*> get <*> get <*> get
+
+instance Binary TyCon where
+  put tc = put (tyConPackage tc) >> put (tyConModule tc) >> put (tyConName tc)
+  get = mkTyCon3 <$> get <*> get <*> get
+
+#endif
 
 -- ------------------------------------------------------------
