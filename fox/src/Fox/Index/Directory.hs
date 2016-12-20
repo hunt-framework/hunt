05466ee32bf3134d8ba3baadf79770e19fd82ba9
Alex Biehl <abiehl@novomind.com>
2016-12-20 16:29:37 +0100

    Start implementation of IndexDirectory

--- fox/src/Fox/Index/Directory.hs
+++ fox/src/Fox/Index/Directory.hs
@@ -1,8 +1,27 @@
 module Fox.Index.Directory where
 
+import Control.Monad.IO.Class
+import Control.Monad.Except
+import System.Directory
+
 newtype IndexDirectory = IndexDirectory { unIndexDirectory :: FilePath }
                        deriving (Eq, Ord, Show)
 
-openIndexDirectory :: FilePath -> IO IndexDirectory
-openIndexDirectory indexDir = do
-  undefined
+data IndexDirErr = ErrInvalidDirectory
+                 | ErrIndexLocked
+
+openIndexDirectory :: FilePath -> IO (Either IndexDirErr IndexDirectory)
+openIndexDirectory indexDir = runExceptT $ do
+    -- bail out early so we don't overwrite anything
+  -- in case indexDir refers to a file.
+  fileExists <- liftIO $ doesFileExist indexDir
+  when fileExists $
+    throwError ErrInvalidDirectory
+
+  -- check explicitly for existence to avoid
+  -- exceptions.
+  dirExists <- liftIO $ doesDirectoryExist indexDir
+  unless dirExists $
+    liftIO $ createDirectoryIfMissing True indexDir
+
+  return undefined
